522cd8f62bc8ea9b102ed313c2d0c42084ffb3b3
Aryan Kashem <aryankashem@gmail.com>
2024-11-04 00:21:26 -0500

    Master tasks setup

---
+++ libraries/master/inc/master_tasks.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/************************************************************************************************
+ * master_tasks.h
+ *
+ * Header file for Master Tasks API. Supports 1KHz, 100Hz and 10Hz scheduling
+ *
+ * Created: 2024-11-04
+ * Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
---
+++ libraries/master/test/test_master_tasks.c
@@ -0,0 +1,14 @@
+/************************************************************************************************
+ * test_master_tasks.h
+ *
+ * Test file for Master Tasks API. Supports 1KHz, 100Hz and 10Hz scheduling
+ *
+ * Created: 2024-11-04
+ * Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
