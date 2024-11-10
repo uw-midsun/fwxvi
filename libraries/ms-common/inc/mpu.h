c142ce7c0ed345c78da2326ded802432d2af1fd4
Megan Lee <leemegan722@gmail.com>
2024-11-10 19:06:37 +0000

    created mpu header and source file

--- libraries/ms-common/inc/mpu.h
+++ libraries/ms-common/inc/mpu.h
@@ -10,7 +10,45 @@
  ************************************************************************************************/
 
 /* Standard library headers */
+#include <stdbool.h>
 
 /* Inter-component Headers */
 
 /* Intra-component Headers */
+#include "status.h"
+
+typedef struct {
+    uint8_t enable;        
+    uint8_t number;
+    uint32_t base_address;
+    uint8_t size;              
+} MPURegion;
+
+typedef struct {
+    uint8_t subregion_disable;
+    uint8_t type_ext_field;
+    uint8_t access_permission;
+    uint8_t disable_exec;
+    uint8_t is_shareable; 
+    uint8_t is_cacheable;
+    uint8_t is_bufferable;
+} MPURegionSettings;
+
+/**
+ * @brief   Configures MPU region with given settings
+ * @param   region Pointer to which MPU region to configure
+ * @param   settings Pointer to MPU region configuration settings
+ */
+StatusCode mpu_configure_region(MPURegion *region, MPURegionSettings *settings);
+
+/**
+ * @brief   Disables Given Region.
+ * @param   region_number Number of MPU region to disable
+ */
+StatusCode mpu_disable_region(uint32_t region_number);
+
+/**
+ * @brief   Enables or Disables MPU with default memory access
+ * @param   enable MPU Status
+ */
+StatusCode mpu_enable(bool enable);
--- libraries/ms-common/src/arm/mpu.c
+++ libraries/ms-common/src/arm/mpu.c
@@ -10,5 +10,41 @@
 /* Standard library headers */
 
 /* Inter-component Headers */
+#include "stm32l4xx_hal.h"
 
 /* Intra-component Headers */
+#include "mpu.h"
+
+StatusCode mpu_configure_region(MPURegion *region, MPURegionSettings *settings){
+    MPU_Region_InitTypeDef MPU_InitStruct;
+
+    MPU_InitStruct.Enable = region->enable;
+    MPU_InitStruct.Number = region->number;
+    MPU_InitStruct.BaseAddress = region->base_address;
+    MPU_InitStruct.Size = region->size;
+    MPU_InitStruct.SubRegionDisable = settings->subregion_disable;
+    MPU_InitStruct.TypeExtField = settings->type_ext_field;
+    MPU_InitStruct.AccessPermission = settings->access_permission;
+    MPU_InitStruct.DisableExec = settings->disable_exec;
+    MPU_InitStruct.IsShareable = settings->is_shareable;
+    MPU_InitStruct.IsCacheable = settings->is_cacheable;
+    MPU_InitStruct.IsBufferable = settings->is_bufferable;
+
+    HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+    return STATUS_CODE_OK;
+}
+
+StatusCode mpu_disable_region(uint32_t region_number){
+    HAL_MPU_DisableRegion(region_number);
+}
+
+StatusCode mpu_enable(bool enable){
+    if (enable) {
+        HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+    } else {
+        HAL_MPU_Disable();
+    }
+
+    return STATUS_CODE_OK;
+}
