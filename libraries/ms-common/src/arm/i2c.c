ed77105cb583fd000d6d2bbf7a43a53cb4d173c8
Brandon Ling <79050338+bzzling@users.noreply.github.com>
2024-11-10 05:34:00 +0000

    mostly complete

--- libraries/ms-common/src/arm/i2c.c
+++ libraries/ms-common/src/arm/i2c.c
@@ -72,6 +72,41 @@ static const uint32_t s_i2c_timing[] = {
 
 static I2C_HandleTypeDef s_i2c_handles[NUM_I2C_PORTS];
 
+// Attempt to recover from a bus lockup
+static StatusCode prv_recover_lockup(I2CPort i2c) {
+  HAL_I2C_DeInit(&s_i2c_handles[i2c]);
+  I2CSettings *settings = &s_port[i2c].settings;
+  // Manually clock SCL
+  gpio_init_pin(&settings->scl, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+  for (size_t i = 0; i < 16; i++) {
+    gpio_toggle_state(&settings->scl);
+  }
+  gpio_init_pin(&settings->scl, GPIO_ALTFN_PUSH_PULL, GPIO_STATE_HIGH);
+  // Clear I2C error flags
+  __HAL_I2C_CLEAR_FLAG(&s_i2c_handles[i2c], I2C_FLAG_BERR | I2C_FLAG_ARLO | I2C_FLAG_OVR | I2C_FLAG_TIMEOUT);
+  // Disable the I2C peripheral
+  __HAL_I2C_DISABLE(&s_i2c_handles[i2c]);
+  // Enable the I2C peripheral
+  __HAL_I2C_ENABLE(&s_i2c_handles[i2c]);
+  // Reinitialize
+  s_i2c_handles[i2c].Instance = s_port[i2c].base;
+  s_i2c_handles[i2c].Init.ClockSpeed = s_i2c_timing[settings->speed];
+  s_i2c_handles[i2c].Init.DutyCycle = I2C_DUTYCYCLE_2;
+  s_i2c_handles[i2c].Init.OwnAddress1 = 0;
+  s_i2c_handles[i2c].Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  s_i2c_handles[i2c].Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  s_i2c_handles[i2c].Init.OwnAddress2 = 0;
+  s_i2c_handles[i2c].Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  s_i2c_handles[i2c].Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&s_i2c_handles[i2c]) != HAL_OK) {
+    return STATUS_CODE_INTERNAL_ERROR;
+  }  
+  if (__HAL_I2C_GET_FLAG(&s_i2c_handles[i2c], I2C_FLAG_BUSY) == SET) {
+      return STATUS_CODE_UNREACHABLE;
+  }
+  return STATUS_CODE_OK;
+}
+
 StatusCode i2c_init(I2CPort i2c, const I2CSettings *settings) {
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
@@ -143,5 +178,107 @@ StatusCode i2c_init(I2CPort i2c, const I2CSettings *settings) {
   // Enable I2C peripheral
   __HAL_I2C_ENABLE(&s_i2c_handles[i2c]);
 
+  return STATUS_CODE_OK;
+}
+
+// Do a transaction on the I2C bus
+static StatusCode prv_txn(I2CPort i2c, I2CAddress addr, uint8_t *data, size_t len, bool read) {
+  // Check that bus is not busy - If it is, assume that lockup has occurred
+  StatusCode status = STATUS_CODE_OK;
+  if (!s_port[i2c].multi_txn && __HAL_I2C_GET_FLAG(&s_i2c_handles[i2c], I2C_FLAG_BUSY) == SET) {
+    status_ok_or_return(prv_recover_lockup(i2c));
+  }
+  queue_reset(&s_port[i2c].i2c_buf.queue);
+  s_port[i2c].current_addr = (addr) << 1;
+  
+  // Set up txn for read or write
+  if (read) {
+    s_port[i2c].curr_mode = I2C_MODE_RECEIVE;
+    s_port[i2c].num_rx_bytes = len;
+  } else {
+    s_port[i2c].curr_mode = I2C_MODE_TRANSMIT;
+    for (size_t tx = 0; tx < len; tx++) {
+      if (queue_send(&s_port[i2c].i2c_buf.queue, &data[tx], 0)) {
+        return STATUS_CODE_RESOURCE_EXHAUSTED;
+      }
+    }
+  }
+
+  // Enable Interrupts before setting start to begin txn
+  s_port[i2c].exit = STATUS_CODE_OK;
+  HAL_I2C_GenerateStop(&s_i2c_handles[i2c], DISABLE); // will need to fix
+  I2C_GenerateSTART(s_port[i2c].base, ENABLE); // will need to fix
+  I2C_ITConfig(s_port[i2c].base, I2C_IT_ERR | I2C_IT_EVT, ENABLE);
+  // Wait for signal that txn is finished from ISR, then disable IT and generate stop
+  status = sem_wait(&s_port[i2c].i2c_buf.wait_txn, I2C_TIMEOUT_MS);
+  I2C_ITConfig(s_port[i2c].base, I2C_IT_ERR | I2C_IT_EVT, DISABLE);
+  if (!s_port[i2c].multi_txn ||
+      status) {  // In an multi exchange or failed txn, need to keep line live to receive data
+    I2C_GenerateSTOP(s_port[i2c].base, ENABLE);
+  }
+}
+
+StatusCode i2c_read(I2CPort i2c, I2CAddress addr, uint8_t *rx_data, size_t rx_len) {
+  if (i2c >= NUM_I2C_PORTS || rx_len > I2C_MAX_NUM_DATA) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
+  // Lock I2C resource
+  StatusCode res = STATUS_CODE_OK;
+  status_ok_or_return(mutex_lock(&s_port[i2c].mutex, I2C_TIMEOUT_MS));
+  s_port[i2c].multi_txn = false;
+  res = prv_txn(i2c, addr, rx_data, rx_len, true);
+  mutex_unlock(&s_port[i2c].mutex);
+  // Return status code ok unless an error has occurred
+  return res;
+}
+
+// Address needs to be just the device address, read/write bit is taken care of in hardware
+StatusCode i2c_write(I2CPort i2c, I2CAddress addr, uint8_t *tx_data, size_t tx_len) {
+  if (i2c >= NUM_I2C_PORTS || tx_len > I2C_MAX_NUM_DATA) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
+  // Lock I2C resource
+  StatusCode res = STATUS_CODE_OK;
+  status_ok_or_return(mutex_lock(&s_port[i2c].mutex, I2C_TIMEOUT_MS));
+  s_port[i2c].multi_txn = false;
+  res = prv_txn(i2c, addr, tx_data, tx_len, false);
+  mutex_unlock(&s_port[i2c].mutex);
+  return res;
+}
+
+StatusCode i2c_read_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *rx_data,
+                        size_t rx_len) {
+  if (i2c >= NUM_I2C_PORTS || rx_len > I2C_MAX_NUM_DATA) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
+
+  StatusCode res = STATUS_CODE_OK;
+  status_ok_or_return(mutex_lock(&s_port[i2c].mutex, I2C_TIMEOUT_MS));
+  uint8_t reg_to_read = reg;
+  s_port[i2c].multi_txn = true;
+  // Write register value, then read result in second txn
+  res = prv_txn(i2c, addr, &reg_to_read, sizeof(reg_to_read), false);
+  if (res) {
+    mutex_unlock(&s_port[i2c].mutex);
+    return res;
+  }
+  res = prv_txn(i2c, addr, rx_data, rx_len, true);
+  mutex_unlock(&s_port[i2c].mutex);
+  return res;
+}
+
+StatusCode i2c_write_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *tx_data,
+                         size_t tx_len) {
+  if (i2c >= NUM_I2C_PORTS || tx_len > I2C_MAX_NUM_DATA - 1) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
+
+  uint8_t write_data[I2C_MAX_NUM_DATA] = { 0 };
+  write_data[0] = reg;
+  for (size_t i = 1; i < tx_len + 1; i++) {
+    write_data[i] = tx_data[i - 1];
+  }
+
+  status_ok_or_return(i2c_write(i2c, addr, write_data, tx_len + 1));
   return STATUS_CODE_OK;
 }
