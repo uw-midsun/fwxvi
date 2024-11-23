71536b213703f65369e35931480b4856b3b2f929
Darren Liu <darrrenliu@gmail.com>
2024-11-23 15:04:47 -0500

    crc

--- libraries/ms-common/inc/crc32.h
+++ libraries/ms-common/inc/crc32.h
@@ -2,3 +2,5 @@
 
 #include <stddef.h>
 #include <stdint.h>
+
+uint32_t calculate_crc(const uint8_t *data, size_t length);
--- libraries/ms-common/src/crc32.c
+++ libraries/ms-common/src/crc32.c
@@ -18,6 +18,5 @@ uint32_t calculate_crc(const uint8_t *data, size_t length){
             }
         }
     }
-
     return ~crc;
 }
