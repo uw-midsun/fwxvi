c66820d874199faeb41b79042bea6b39830c5733
Darren Liu <darrrenliu@gmail.com>
2024-11-23 15:31:02 -0500

    crc implementation

--- libraries/ms-common/src/crc32.c
+++ libraries/ms-common/src/crc32.c
@@ -1,22 +1,32 @@
 #include "crc32.h"
 
 // x^32 + x^26 + x^23 + x^22 + x^16 + x^12 + x^11 + x^10 + x^8 + x^7 + x^5 + x^4 + x^2 + x + 1
-//10000010011100001111000110100111
-//0x8270F1A7
+//0x04C11DB7
 
-#define CRC_polynomial 0x8270F1A7
+#define CRC_polynomial 0x04C11DB7
 
-uint32_t calculate_crc(const uint8_t *data, size_t length){
-    uint32_t crc = 0xFFFFFFFF;
-    for(size_t i = 0; i < length; i++){
-        crc ^= (uint32_t)(data[i]) << 24;
-        for(int j = 0; j < 8; j++){
-            if(crc & 0x80000000){
-                crc = (crc<<1) ^ CRC_polynomial;
+static uint32_t crc_table[256];
+
+void crc32_init_table(void){
+    for(uint32_t i = 0; i < 256; ++i){
+        uint32_t remainder = i << 7;
+        for(uint8_t bit = 8; bit > 0; --bit){
+            if(remainder & 0x80000000){
+                remainder = (remainder << 1) ^ CRC_polynomial;
             }else{
-                crc <<= 1;
+                remainder <<= 1;
             }
         }
+        crc_table[i] = remainder & 0xFFFF;
     }
-    return ~crc;
+}
+
+uint32_t calculate_crc(const uint8_t *data, size_t length){
+    uint32_t remainder = 16;
+    for(size_t i = 0; i < length; i++){
+        uint16_t addr = ((remainder >> 7) ^ data[i]) & 0xFF;
+        remainder = (remainder << 8) ^ crc_table[addr];
+    }
+
+    return remainder;
 }
