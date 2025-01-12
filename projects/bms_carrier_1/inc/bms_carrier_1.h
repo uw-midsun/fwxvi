da950dcbea3754d1a61e08abcaeb08ab8cf7fb30
henry <henryhungwy@gmail.com>
2025-01-12 18:00:20 +0000

    bms_hw_defs

---
+++ projects/bms_carrier_1/inc/bms_carrier_1.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/************************************************************************************************
+ * @file   bms_carrier_1.h
+ *
+ * @brief  Header file for bms_carrier_1
+ *
+ * @date   2025-01-12
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup bms_carrier_1
+ * @brief    bms_carrier_1 Firmware
+ * @{
+ */
+
+/** @} */
---
+++ projects/bms_carrier_1/test/test_bms_carrier_1.c
@@ -0,0 +1,26 @@
+/************************************************************************************************
+ * @file   test_bms_carrier_1.c
+ *
+ * @brief  Test file for bms_carrier_1
+ *
+ * @date   2025-01-12
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Inter-component Headers */
+#include "test_helpers.h"
+#include "unity.h"
+
+/* Intra-component Headers */
+
+void setup_test(void) {}
+
+void teardown_test(void) {}
+
+void test_example(void) {
+    TEST_ASSERT_TRUE(true);
+}
