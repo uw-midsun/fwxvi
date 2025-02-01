004c1643e06f363fdaa59d846cc086fb4d174c9a
Jarvis <jarvis@uwmidsun.com>
2025-02-01 18:04:57 +0000

    I2C working

--- smoke/i2c_api/src/main.c
+++ smoke/i2c_api/src/main.c
@@ -22,24 +22,32 @@
 
 I2CSettings i2c_test_settings = {
   .speed = I2C_SPEED_STANDARD,
-  .sda = {.port = GPIO_PORT_A, .pin = 10},
-  .scl = {.port = GPIO_PORT_A, .pin = 9}
+  .sda = {.port = GPIO_PORT_B, .pin = 11},
+  .scl = {.port = GPIO_PORT_B, .pin = 10}
 };
 
 TASK(i2c_api, TASK_STACK_1024) {
-
-
-  i2c_init(I2C_PORT_1, &i2c_test_settings);
-  I2CAddress address = 100;
+  i2c_init(I2C_PORT_2, &i2c_test_settings);
+  I2CAddress address = 0x45;
   uint8_t data[5] = {0x10, 0x11, 0x12, 0x13, 0x14};
   size_t length = 5;
 
-
-
   while (true) {
-    i2c_write(I2C_PORT_1, address, data, length);
+    LOG_DEBUG("WRITE\n");
+    i2c_write(I2C_PORT_2, address, data, length);
     delay_ms(500);
-    
+
+    LOG_DEBUG("WRITE REG\n");
+    i2c_write_reg(I2C_PORT_2, address, 0x12, data, length);
+    delay_ms(500);
+
+    LOG_DEBUG("READ\n");
+    i2c_read(I2C_PORT_2, address, data, length);
+    delay_ms(500);
+
+    LOG_DEBUG("READ REG\n");
+    i2c_read_reg(I2C_PORT_2, address, 0x12, data, length);
+    delay_ms(500);
   }
 }
 
