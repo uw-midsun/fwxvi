fcf010da6a983b0856e6388529285c2d35848288
M. Wasif Kamran <muwasifk@hotmail.com>
2025-02-01 04:34:12 +0000

    added imu_hw_defs

---
+++ projects/imu/src/main.c
@@ -0,0 +1,41 @@
+/************************************************************************************************
+ * @file   main.c
+ *
+ * @brief  Main file for imu
+ *
+ * @date   2025-02-01
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+#include "mcu.h"
+#include "gpio.h"
+#include "log.h"
+#include "tasks.h"
+#include "master_tasks.h"
+
+/* Intra-component Headers */
+#include "imu.h"
+
+void pre_loop_init() {}
+
+void run_1000hz_cycle() {}
+
+void run_10hz_cycle() {}
+
+void run_1hz_cycle() {}
+
+int main() {
+  mcu_init();
+  tasks_init();
+  log_init();
+
+  init_master_tasks();
+
+  tasks_start();
+
+  LOG_DEBUG("exiting main?");
+  return 0;
+}
