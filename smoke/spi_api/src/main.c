68c9b130136f7ec275db0b8fc54c1576013fd9ff
Jarvis <jarvis@uwmidsun.com>
2025-02-01 21:19:01 +0000

    Working spi

--- smoke/spi_api/src/main.c
+++ smoke/spi_api/src/main.c
@@ -16,11 +16,38 @@
 #include "tasks.h"
 #include "status.h"
 #include "delay.h"
+#include "spi.h"
 
 /* Intra-component Headers */
 
+#define TEST_DATA_SIZE  5U
+
+SpiSettings spi_test_settings = {
+  .baudrate = SPI_BAUDRATE_2_5MHZ,
+  .mode = SPI_MODE_3,
+  .sdo = { .port = GPIO_PORT_B, .pin = 15 },
+  .sdi = { .port = GPIO_PORT_B, .pin = 14 },
+  .sclk = { .port = GPIO_PORT_B, .pin = 13 },
+  .cs = { .port = GPIO_PORT_B, .pin = 12 },
+};
+
 TASK(spi_api, TASK_STACK_1024) {
-  while (true) {}
+  spi_init(SPI_PORT_2, &spi_test_settings);
+
+  uint8_t tx_data[TEST_DATA_SIZE] = {0x10, 0x11, 0x12, 0x13, 0x14};
+  uint8_t rx_data[TEST_DATA_SIZE];
+
+  while (true) {
+    LOG_DEBUG("Exchanging data\n");
+    spi_exchange(SPI_PORT_2, tx_data, sizeof(tx_data), rx_data, sizeof(rx_data));
+
+    for (size_t i = 0U; i < TEST_DATA_SIZE; i++) {
+      LOG_DEBUG("Received byte %d: 0x%x\n", i, rx_data[i]);
+      delay_ms(5);
+    }
+  
+    delay_ms(500);
+  }
 }
 
 int main() {
