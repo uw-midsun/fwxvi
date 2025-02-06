416476f6532fac69965e9bfcb4e950b48187b392
Adityya Kaushal <adityyakaushal@gmail.com>
2025-02-05 19:11:27 -0500

    started pedal code

---
+++ projects/centre_console/inc/pedal.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/************************************************************************************************
+ * @file   pedal.h
+ *
+ * @brief  Header file for pedal in centre_console
+ *
+ * @date   2025-02-02
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup pedal
+ * @brief    pedal for centre_console Firmware
+ * @{
+ */
+
+/** @} */
