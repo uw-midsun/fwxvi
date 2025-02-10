17ce6955a0c7c67097493a6e1ab76d19815c88b5
Jarvis <jarvis@uwmidsun.com>
2025-02-10 22:09:09 +0000

    FSM API

--- libraries/ms-common/inc/fsm.h
+++ libraries/ms-common/inc/fsm.h
@@ -28,6 +28,8 @@
 
 #define MAX_STATES 10
 #define MAX_TRANSITIONS 5
+#define FSM_TIMEOUT_MS 1000
+#define CYCLE_RX_MAX 15
 
 typedef void (*StateAction)(void *context);
 typedef uint8_t StateId;
@@ -48,10 +50,29 @@ typedef struct {
   uint8_t num_states;
 } Fsm;
 
+#define FSM(name, num_fsm_states, stack_size) \
+  Fsm *name##_fsm = &((Fsm){                  \
+      .num_states = num_fsm_states,           \
+  });                                         \
+  TASK(name, stack_size) {                    \
+    _fsm_task(context);                       \
+  }
+
+  #define TRANSITION(from, to);
 /**
  * @brief   Creates state with associated ID in a state list
  * @details State id must be unique (preferred to use enum type)
  */
-#define STATE(state_id, entry, state_func) [state_id] = { .entry_func = entry, .state_action = state_func }
+#define STATE(StateId, inputs, outputs) \
+  { .id = STATE_ID, .inputs = entry_func, .outputs = state_action }
 
-/** @} */
+StatusCode fsm_init(Fsm *fsm, State *states, uint8_t *transitions, StateId initial_state, void *context);
+
+StatusCode fsm_transition(Fsm *fsm, StateId from, StateId to);
+
+void fsm_run_cycle(Fsm *fsm);
+
+void _fsm_task(void *context);
+
+StatusCode fsm_add_state(Fsm *fsm, StateId state_id, StateAction entry_func, StateAction state_func);
+StatusCode fsm_add_transition(Fsm *fsm, StateId from, StateId to);
