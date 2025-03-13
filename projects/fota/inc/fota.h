67c8ef7fb1de9a30bdfa587eae6c33e3e55a2d50
Aryan Kashem <130705280+Akashem06@users.noreply.github.com>
2025-03-12 23:43:25 -0400

    FOTA (#43)
    Co-authored-by: Jarvis <jarvis@uwmidsun.com>

---
+++ projects/fota/inc/fota.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/************************************************************************************************
+ * @file   fota.h
+ *
+ * @brief  Header file for fota
+ *
+ * @date   2025-03-12
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup fota
+ * @brief    fota Firmware
+ * @{
+ */
+
+/** @} */
