d5540546b3b5e48b6617301411f551f85de0b88d
M. Wasif Kamran <115906305+muwasifk@users.noreply.github.com>
2025-04-28 11:03:55 -0400

    IMU Firmware (#27)
    * added imu_hw_defs
    
    * make structs
    
    * ported old driver
    
    * formatting
    
    * updated imu init
    
    * fix SPI
    
    * refactor SPI
    
    * fix files
    
    * suggested changes
    
    * add imu_init declaration
    
    * fix build issue
    
    * restructure imu_init
    
    * add declarations
    
    * format
    
    * format
    
    * fix build errors
    
    * fix build errors
    
    * fix build errors
    
    * fix build errors
    
    * IMU FIXES
    
    * IMU CHANGES
    
    * finished testing imu
    
    * fixed formatting
    
    * Fix Bmi323Registers naming
    
    ---------
    
    Co-authored-by: Jarvis <jarvis@uwmidsun.com>

---
+++ projects/imu/src/imu.c
@@ -0,0 +1,45 @@
+/************************************************************************************************
+ * @file   imu.c
+ *
+ * @brief  driver file for imu
+ *
+ * @date   2025-02-01
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdio.h>
+
+/* Inter-component Headers */
+#include "can.h"
+#include "status.h"
+#include "system_can.h"
+
+/* Intra-component Headers */
+#include "imu.h"
+#include "imu_hw_defs.h"
+
+static CanStorage s_can_storage = { 0U };
+const CanSettings s_can_settings = {
+  .device_id = SYSTEM_CAN_DEVICE_IMU,
+  .bitrate = CAN_HW_BITRATE_500KBPS,
+  .tx = IMU_CAN_TX,
+  .rx = IMU_CAN_RX,
+  .loopback = 0,
+};
+
+StatusCode imu_init(Bmi323Storage *storage, Bmi323Settings *settings) {
+  if (storage == NULL || settings == NULL) {
+    return STATUS_CODE_INVALID_ARGS;
+  }
+
+  storage->settings = settings;
+
+  status_ok_or_return(spi_init(settings->spi_port, &settings->spi_settings));
+
+  //   status_ok_or_return(can_init(&s_can_storage, &s_can_settings));
+
+  status_ok_or_return(bmi323_init(storage));
+
+  return STATUS_CODE_OK;
+}
