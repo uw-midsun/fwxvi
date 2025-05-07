35756cb821308686e0f26bc3879fbc07e29b2948
DaryanZabear <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-05-07 19:27:31 -0400

    Finished smoke test

--- smoke/ltc6811/src/main.c
+++ smoke/ltc6811/src/main.c
@@ -18,9 +18,79 @@
 #include "delay.h"
 
 /* Intra-component Headers */
+#include "ltc_afe.h"
 
 TASK(ltc6811, TASK_STACK_1024) {
-  while (true) {}
+  LtcAfeStorage afe_storage; 
+
+  /* Based on spi smoke test (Not sure) */
+  const SpiSettings spi_config = {
+    .baudrate = SPI_BAUDRATE_2_5MHZ,
+    .mode     = SPI_MODE_3,
+    .sdo      = { .port = GPIO_PORT_B, .pin = 15 },
+    .sdi      = { .port = GPIO_PORT_B, .pin = 14 },
+    .sclk     = { .port = GPIO_PORT_B, .pin = 13 },
+    .cs       = { .port = GPIO_PORT_B, .pin = 12 },
+  };
+
+  LtcAfeSettings afe_settings = {
+    .adc_mode = LTC_AFE_ADC_MODE_27KHZ,
+    
+    .cell_bitset[0] = 0x0FFFU,
+    .aux_bitset[0]  = 0x00FFU,
+
+    .num_devices = 3, 
+    .num_cells = LTC_AFE_MAX_CELLS, 
+    .num_thermistors = LTC_AFE_MAX_THERMISTORS, 
+
+    .result_context = NULL, 
+    .spi_settings = spi_config, 
+    .spi_port = SPI_PORT_2 /* Not sure */
+  };
+
+  StatusCode status = ltc_afe_init(&afe_storage, &afe_settings);
+  if (status != STATUS_CODE_OK) {
+    LOG_DEBUG("ltc_afe_init() failed: %d\n", status);
+    while (true){}                               
+  }
+
+  LOG_DEBUG("LTC6811 init OK – beginning conversions…\n");
+
+  while (true) {
+    status = ltc_afe_trigger_cell_conv(&afe_storage); 
+    if (status != STATUS_CODE_OK) {
+      LOG_DEBUG("ADCV (Trigger Cell Voltage Conversion) failed: %d\n", status);
+      delay_ms(5);
+      continue;
+    }
+    
+    status = ltc_afe_read_cells(&afe_storage); 
+    if (status != STATUS_CODE_OK) {
+      LOG_DEBUG("RDCV (Read cell conversion) failed: %d\n", status);
+      delay_ms(5);
+      continue;
+    }
+
+    status = ltc_afe_trigger_aux_conv(&afe_storage, 4); 
+    if (status != STATUS_CODE_OK) {
+      LOG_DEBUG("ADAX (Trigger aux Voltage Conversion) failed: %d\n", status);
+      delay_ms(5);
+      continue;
+    }
+    
+    status = ltc_afe_read_aux(&afe_storage, 4); 
+    if (status != STATUS_CODE_OK) {
+      LOG_DEBUG("RDAUX (Read aux conversion) failed: %d\n", status);
+      delay_ms(5);
+      continue;
+    }
+    
+    for (size_t i = 0; i < LTC_AFE_MAX_CELLS; i++) {
+      LOG_DEBUG("Cell %02u : %4u \n", (unsigned)i,
+                (unsigned)(afe_storage.cell_voltages));
+      delay_ms(5);
+    }
+  }
 }
 
 int main() {
