c03f7a44f67a71552b4189af64f325203e6a0617
Jarvis <jarvis@uwmidsun.com>
2025-05-12 23:13:59 +0000

    DAC Abstraction

---
+++ libraries/ms-common/inc/dac.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/************************************************************************************************
+ * @file    dac.h
+ *
+ * @brief   DAC Library Header file
+ *
+ * @date    2025-05-12
+ * @author  Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup dac
+ * @brief    dac Firmware
+ * @{
+ */
+
+
+/** @} */
---
+++ libraries/ms-common/src/arm/dac.c
@@ -0,0 +1,14 @@
+/************************************************************************************************
+ * @file    dac.c
+ *
+ * @brief   DAC Library Source file
+ *
+ * @date    2025-05-12
+ * @author  Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
