853f5d459e7ce63546ce6f0a1bc2d759e4f1e481
Aditya Sen <a5sen@uwaterloo.ca>
2025-05-28 19:50:19 -0400

    Rear controller (#64)
    * Create rear controller board project
    
    * Format + lint files for rear controller
    
    * Add rear controller yaml file
    
    ---------
    
    Co-authored-by: Jarvis <jarvis@uwmidsun.com>

---
+++ projects/rear_controller/inc/rear_fw_defs.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/************************************************************************************************
+ * @file   rear_fw_defs.h
+ *
+ * @brief  Rear controller board hardware definitions
+ *
+ * @date   2025-05-28
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup Rear Controller
+ * @brief    Rear Controller Board Firmware
+ * @{
+ */
+
+/** @} */
