f2da315cf5ad9b8ac629df0bb6c1f0118be0104c
Roshan Hegde <r3hegde@uwaterloo.ca>
2025-05-28 15:54:53 -0400

    dfu bootloader rough implementation

--- projects/fota/src/fota_dfu.c
+++ projects/fota/src/fota_dfu.c
@@ -1,5 +1,6 @@
-#include "fota_datagram.h"
 #include "fota_dfu.h"
+
+#include "fota_datagram.h"
 #include "fota_memory_map.h"
 #include "network.h"
 #include "network_buffer.h"
@@ -13,12 +14,12 @@ static UartSettings pacman_settings = {
   .baudrate = 1024                                   // paceholder baud rate
 };
 
+FotaError fota_dfu_init(PacketManager *packet_manager) {
+  if (packet_manager == NULL) {
+    return FOTA_ERROR_BOOTLOADER_PACKET_MANAGER_UNINITIALIZED;
+  }
 
-FotaError fota_dfu_init(void) {
   data.curr_state = DFU_UNINITALIZED;
-
-  if (packet_manager_init(&pacman, &pacman_settings, NULL) != FOTA_ERROR_SUCCESS) return FOTA_ERROR_INTERNAL_ERROR;
-
   data.application_start = APPLICATION_START_ADDRESS;
   data.binary_size = FLASH_SIZE_LINKERSCRIPT;
   data.buffer_index = 0;
@@ -39,10 +40,75 @@ FotaError run_dfu_bootloader() {
      4. check for error and exit
    */
 
-  
+  FotaError err = FOTA_ERROR_BOOTLOADER_SUCCESS;
+
+  // Get a datagram from the packet manager
+  FotaDatagram *datagram = packet_manager_get_datagram(&pacman, data.expected_datagram_id, datagram);
+  if (datagram == NULL) {
+    return FOTA_ERROR_SUCCESS;
+  }
+
+  // update state based on datagram type
+  switch (datagram->header.type) {
+    case FOTA_DATAGRAM_TYPE_FIRMWARE_METADATA:
+      // Process metadata
+
+      break;
+    case FOTA_DATAGRAM_TYPE_FIRMWARE_CHUNK:
+
+      break;
+
+    case FOTA_DATAGRAM_TYPE_UPDATE_REQUEST:
+      break;
+
+    case FOTA_DATAGRAM_TYPE_VERIFICATION:
+      break;
+    default:
+      break;
+  }
+  if (err != FOTA_ERROR_SUCCESS) {
+      process_new_state(DFU_FAULT);
+      return err;
+  }
 }
 
-static FotaError process_new_state(DFUStates new_state) {
+
+  static onStateChange() {
+    if (data.curr_state == DFU_UNINITALIZED) {
+      return FOTA_ERROR_BOOTLOADER_UNINITIALIZED;
+    }
+
+    switch (data.curr_state) {
+      case DFU_IDLE:
+        // Handle idle state
+        break;
+
+      case DFU_START:
+        // Handle start state
+        break;
+
+      case DFU_DATA_READY:
+        // Handle data ready state
+        break;
+
+      case DFU_DATA_RECEIVE:
+        // Handle data receive state
+        break;
+
+      case DFU_JUMP_APP:
+        // Handle jump to application state
+        break;
+
+      case DFU_FAULT:
+        // Handle fault state
+        return FOTA_ERROR_BOOTLOADER_INVALID_STATE;
+
+      default:
+        return FOTA_ERROR_BOOTLOADER_INVALID_STATE;
+    }
+  }
+
+  static FotaError process_new_state(DFUStates new_state) {
 // Macro on success and failure
 #define SUCCEED()              \
   data.curr_state = new_state; \
@@ -50,66 +116,64 @@ static FotaError process_new_state(DFUStates new_state) {
 
 #define FAIL() return FOTA_ERROR_BOOTLOADER_INVALID_STATE;
 
-  if (data.curr_state == DFU_UNINITALIZED) {
-    return FOTA_ERROR_BOOTLOADER_UNINITIALIZED;
-  }
+    if (data.curr_state == DFU_UNINITALIZED) {
+      return FOTA_ERROR_BOOTLOADER_UNINITIALIZED;
+    }
 
-  if (new_state == data.curr_state) return FOTA_ERROR_BOOTLOADER_SUCCESS;
+    if (new_state == data.curr_state) return FOTA_ERROR_BOOTLOADER_SUCCESS;
 
-  switch (data.curr_state) {
-    case (DFU_UNINITALIZED):
-      switch (new_state) {
-        case DFU_DATA_READY:
-        case DFU_IDLE:
-          SUCCEED();
-        default:
-          FAIL();
-      }
+    switch (data.curr_state) {
+      case (DFU_UNINITALIZED):
+        switch (new_state) {
+          case DFU_DATA_READY:
+          case DFU_IDLE:
+            SUCCEED();
+          default:
+            FAIL();
+        }
 
-    case (DFU_IDLE):
-      switch (new_state) {
-        case DFU_START:
-        case DFU_JUMP_APP:
-        case DFU_FAULT:
-          SUCCEED();
-        default:
-          FAIL()
-      }
+      case (DFU_IDLE):
+        switch (new_state) {
+          case DFU_START:
+          case DFU_JUMP_APP:
+          case DFU_FAULT:
+            SUCCEED();
+          default:
+            FAIL()
+        }
 
-    case (DFU_START):
-      switch (new_state)
-      {
-      case DFU_DATA_READY:
-      case DFU_JUMP_APP:
-      case DFU_FAULT:
+      case (DFU_START):
+        switch (new_state) {
+          case DFU_DATA_READY:
+          case DFU_JUMP_APP:
+          case DFU_FAULT:
+            SUCCEED();
+          default:
+            FAIL();
+        }
+
+      case (DFU_DATA_READY):
         SUCCEED();
-      default:
-        FAIL();
-      }
 
-    case (DFU_DATA_READY):
-      SUCCEED();
-
-    case (DFU_DATA_RECEIVE):
-      switch (new_state)
-      {
-      case DFU_DATA_READY:
-      case DFU_JUMP_APP:
-      case DFU_START:
-      case DFU_FAULT:
-        SUCCEED();
+      case (DFU_DATA_RECEIVE):
+        switch (new_state) {
+          case DFU_DATA_READY:
+          case DFU_JUMP_APP:
+          case DFU_START:
+          case DFU_FAULT:
+            SUCCEED();
+          default:
+            FAIL();
+        }
+      case (DFU_JUMP_APP):
+        switch (new_state) {
+          case DFU_FAULT:
+            SUCCEED();
+          default:
+            FAIL();
+        }
       default:
-        FAIL();
-      } 
-    case (DFU_JUMP_APP):
-      switch (new_state){
-        case DFU_FAULT:
-          SUCCEED();
-        default:
-          FAIL();
-      }
-    default:
-      data.curr_state = DFU_FAULT;
-      return FOTA_ERROR_INVALID_ARGS;
+        data.curr_state = DFU_FAULT;
+        return FOTA_ERROR_INVALID_ARGS;
+    }
   }
-}
