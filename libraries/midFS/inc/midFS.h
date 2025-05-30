2767a3904c31a65d8955c9d11225098fd0a38486
Darren Liu <darrrenliu@gmail.com>
2025-05-29 20:55:02 -0400

    function headers

--- libraries/midFS/inc/midFS.h
+++ libraries/midFS/inc/midFS.h
@@ -3,9 +3,18 @@
 
 SuperBlock superBlock = {
     .magic = 0xC1D1921D,
-    .maxFiles = MAX_FILES_PER_GROUP * BLOCKS_PER_GROUP * TOTAL_BLOCKS,
+    .maxFiles = MAX_FILES_PER_GROUP * BLOCKS_PER_GROUP * BLOCK_SIZE,
     .blockSize = BLOCK_SIZE,
     .blocksPerGroup = BLOCKS_PER_GROUP,
     .totalSize = superBlock.maxFiles,
-    .rootIndex = 0
+    .rootIndex = 0,
+    .numBlocks = 0
 };
+
+
+void fs_init();
+void fs_add_file(const char * name, uint32_t size, uint8_t isFolder);
+void fs_delete_file(const char *name);
+FileEntry fs_read_file(const char * name);
+uint8_t *fs_extract_file(uint16_t address);
+void fs_write_file(const char * name, uint8_t * content, uint32_t contentSize);
