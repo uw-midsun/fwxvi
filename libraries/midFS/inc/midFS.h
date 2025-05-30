d632eda12721370014839977d616999c207145e5
Darren Liu <darrrenliu@gmail.com>
2025-05-29 21:09:28 -0400

    create block group function

--- libraries/midFS/inc/midFS.h
+++ libraries/midFS/inc/midFS.h
@@ -6,13 +6,14 @@ SuperBlock superBlock = {
     .maxFiles = MAX_FILES_PER_GROUP * BLOCKS_PER_GROUP * BLOCK_SIZE,
     .blockSize = BLOCK_SIZE,
     .blocksPerGroup = BLOCKS_PER_GROUP,
-    .totalSize = superBlock.maxFiles,
+    .totalSize = MAX_FILES_PER_GROUP * BLOCKS_PER_GROUP * BLOCK_SIZE,
     .rootIndex = 0,
     .numBlocks = 0
 };
 
 
 void fs_init();
+void fs_create_block_group();
 void fs_add_file(const char * name, uint32_t size, uint8_t isFolder);
 void fs_delete_file(const char *name);
 FileEntry fs_read_file(const char * name);
