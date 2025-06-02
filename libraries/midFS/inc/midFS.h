6b7e6d877d5020e701a32bcecf3727ffe343740c
Darren Liu <darrrenliu@gmail.com>
2025-06-02 00:25:26 -0400

    fs write file function

--- libraries/midFS/inc/midFS.h
+++ libraries/midFS/inc/midFS.h
@@ -10,12 +10,11 @@ uint32_t fs_create_block_group();
 void fs_read_block_group(uint32_t blockIndex, BlockGroup *dest);
 void fs_write_block_group(uint32_t blockIndex, BlockGroup *src);
 
-void fs_add_file(const char * name, uint8_t* content, uint32_t size, uint8_t isFolder);
-void fs_add_folder(const char *name);
-void fs_delete_file(const char *name);
-uint32_t fs_read_file(const char * name);
-uint8_t *fs_extract_file(uint16_t address);
-void fs_write_file(const char * name, uint8_t * content, uint32_t contentSize);
+void fs_add_file(const char * path, uint8_t* content, uint32_t size, uint8_t isFolder);
+void fs_delete_file(const char* path);
+uint32_t fs_read_file(const char *path);
+void fs_extract_file(uint8_t *addr, uint32_t size);
+void fs_write_file(const char * path, uint8_t * content, uint32_t contentSize);
 
 void fs_split_path(char *path, char *folderPath, char *fileName);
 uint32_t fs_resolve_path(const char *folderPath);
