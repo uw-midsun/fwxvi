e9e182028a8a04b0f0d039c286905c06265a6203
Jarvis <jarvis@uwmidsun.com>
2025-06-05 00:26:55 +0000

    created file system project and test file system file

---
+++ projects/file_system/test/test_file_system.c
@@ -0,0 +1,28 @@
+/************************************************************************************************
+ * @file   test_file_system.c
+ *
+ * @brief  Test file for file_system
+ *
+ * @date   2025-06-05
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Inter-component Headers */
+#include "test_helpers.h"
+#include "unity.h"
+#include "file_system.h"
+/* External library Headers */
+
+/* Intra-component Headers */
+
+void setup_test(void) {}
+
+void teardown_test(void) {}
+
+void test_example(void) {
+    TEST_ASSERT_TRUE(true);
+}
