33ba1ca96be7683df7fc716c8459a71b396cc2af
Daiyan Zubaier <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-07-04 21:21:34 -0400

    Sd Card Driver implementation (#67)
    * Reimplemented changes from old branch
    
    * Formatting build fixes
    
    * PR requested changes completed
    
    * Fix format
    
    * Fix x86 build
    
    ---------
    
    Co-authored-by: Jarvis <jarvis@uwmidsun.com>
    Co-authored-by: Aryan Kashem <aryankashem@gmail.com>

---
+++ projects/telemetry/src/x86/sd_card_spi.c
@@ -0,0 +1,76 @@
+/************************************************************************************************
+ * @file    sd_card_spi.c
+ *
+ * @brief   Sd Card Spi
+ *
+ * @date    2025-06-07
+ * @author  Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdint.h>
+#include <string.h>
+
+/* Inter-component Headers */
+#include "delay.h"
+#include "gpio.h"
+#include "log.h"
+
+/* Intra-component Headers */
+#include "sd_card_spi.h"
+
+/* Standard Header*/
+
+/* Macros and Defines*/
+#define SD_TOKEN_START_BLOCK 0xFE
+#define SD_TOKEN_MULTI_WRITE 0xFC
+#define SD_TOKEN_STOP_TRAN 0xFD
+
+#define R1_IDLE 0x01
+#define R1_ILLEGAL 0x04
+
+#define DATA_ACCEPTED 0x05
+
+#define CRC_CMD0 0x95 /* valid CRC for CMD0 arg = 0   */
+#define CRC_CMD8 0x87 /* valid CRC for CMD8 arg = 0x1AA */
+
+#define T_INIT_MS 1000U
+#define T_RW_MS 250U
+
+#define SD_NUM_RETRIES 8
+
+#define ACMD41_ARG_V2 0x40000000U
+#define ACMD41_ARG_V1 0U
+
+#define CMD(n) (0x40 | (n))
+enum {
+  CMD0_GO_IDLE = 0,
+  CMD8_SEND_IF_COND = 8,
+  CMD13_SEND_STATUS = 13,
+  CMD16_SET_BLKLEN = 16,
+  CMD17_READ_SINGLE = 17,
+  CMD24_WRITE_SINGLE = 24,
+  CMD25_WRITE_MULTI = 25,
+  CMD55_APP_CMD = 55,
+  CMD58_READ_OCR = 58,
+  ACMD23_PRE_ERASE = 23,
+  ACMD41_SD_OP_COND = 41
+};
+
+/*  Public API
+------------------------------------------------------------------ */
+StatusCode sd_card_spi_init(SdSpiPort p, SdSpiSettings *settings) {
+  return STATUS_CODE_UNIMPLEMENTED;
+}
+
+StatusCode sd_read_blocks(SdSpiPort p, uint8_t *dst, uint32_t lba, uint32_t number_of_blocks) {
+  return STATUS_CODE_UNIMPLEMENTED;
+}
+
+StatusCode sd_write_blocks(SdSpiPort p, uint8_t *src, uint32_t lba, uint32_t number_of_blocks) {
+  return STATUS_CODE_UNIMPLEMENTED;
+}
+
+StatusCode sd_is_initialized(SdSpiPort p) {
+  return STATUS_CODE_UNIMPLEMENTED;
+}
