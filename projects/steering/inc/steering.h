3422d549527c4d22dbf5bd2d2deee9f6735ee3d1
Aditya Sen <a5sen@uwaterloo.ca>
2025-07-09 19:40:55 -0400

    Add steering project (#73)

---
+++ projects/steering/inc/steering.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/************************************************************************************************
+ * @file   steering.h
+ *
+ * @brief  Header file for steering
+ *
+ * @date   2025-07-09
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup steering
+ * @brief    steering Firmware
+ * @{
+ */
+
+/** @} */
