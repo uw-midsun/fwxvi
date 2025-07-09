ce7e11769a7396d76d1f792fe4f45f97e2a292fb
Aditya Sen <a5sen@uwaterloo.ca>
2025-07-09 19:40:55 -0400

    Add steering project (#73)

---
+++ projects/steering/test/test_steering.c
@@ -0,0 +1,26 @@
+/************************************************************************************************
+ * @file   test_steering.c
+ *
+ * @brief  Test file for steering
+ *
+ * @date   2025-07-09
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Inter-component Headers */
+#include "test_helpers.h"
+#include "unity.h"
+
+/* Intra-component Headers */
+
+void setup_test(void) {}
+
+void teardown_test(void) {}
+
+void test_example(void) {
+    TEST_ASSERT_TRUE(true);
+}
