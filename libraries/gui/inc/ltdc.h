93464f410294603688146fa267532760f58c3f70
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-07-10 01:04:24 +0000

    Set up ltdc.c/h

--- libraries/gui/inc/ltdc.h
+++ libraries/gui/inc/ltdc.h
@@ -10,15 +10,31 @@
  ************************************************************************************************/
 
 /* Standard library Headers */
+#include <stdint.h>
 
 /* Inter-component Headers */
 
 /* Intra-component Headers */
+#define LTDC
+#include <stm32l4xx_hal_ltdc.h>
 
 /**
  * @defgroup GUI
  * @brief    GUI Firmware
  * @{
  */
+
+typedef struct {
+    uint16_t width;
+    uint16_t height;
+    uint8_t *framebuffer;
+    uint32_t *clut;
+
+    LTDC_LayerCfgTypeDef layer_cfg;   
+} LtdcConfig;
+
+void init_ltdc(void);
+void set_framebuffer(uint8_t *buffer);
+void load_clut(void);
 
 /** @} */
