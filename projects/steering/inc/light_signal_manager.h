b83895beab2969bef6917d7e6c79fe65a8467a23
Afnan-Alam <afnan.alam1971@gmail.com>
2025-07-18 09:25:35 -0400

    Initial Commit

---
+++ projects/steering/inc/light_signal_manager.h
@@ -0,0 +1,69 @@
+#pragma once
+
+/************************************************************************************************
+ * @file    light_signal_manager.h
+ *
+ * @brief   Header file for light signal manager (turn signals, hazard)
+ *
+ * @date    2025-07-16
+ * @author  Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdbool.h>
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup LIGHTS_SIGNAL_MANAGER
+ * @brief    Controls light signals for left, right, and hazard indications
+ * @{
+ */
+
+/**
+ * @brief Light signal states representing the current output
+ */
+typedef enum {
+    LIGHTS_SIGNAL_STATE_OFF = 0,    /**< All lights off */
+    LIGHTS_SIGNAL_STATE_LEFT,       /**< Left signal active */
+    LIGHTS_SIGNAL_STATE_RIGHT,      /**< Right signal active */
+    LIGHTS_SIGNAL_STATE_HAZARD      /**< Hazard signal active */
+} LightsSignalState;
+
+/**
+ * @brief Requests made to control the light signals
+ */
+typedef enum {
+    LIGHTS_SIGNAL_REQUEST_OFF = 0,    /**< Request to turn off signals */
+    LIGHTS_SIGNAL_REQUEST_LEFT,       /**< Request to turn on left signal */
+    LIGHTS_SIGNAL_REQUEST_RIGHT,      /**< Request to turn on right signal */
+    LIGHTS_SIGNAL_REQUEST_HAZARD      /**< Request to turn on hazard signal */
+} LightsSignalRequest;
+
+/**
+ * @brief Initialize the light signal manager
+ */
+void lights_signal_manager_init(void);
+
+/**
+ * @brief Make a request to change the signal state
+ *
+ * @param req The requested light signal state
+ */
+void lights_signal_manager_request(LightsSignalRequest req);
+
+/**
+ * @brief Update the signal state based on the current request
+ */
+void lights_signal_manager_update(void);
+
+/**
+ * @brief Get the current state of the signal lights
+ *
+ * @return The current light signal state
+ */
+LightsSignalState lights_signal_manager_get_state(void);
+
+/** @} */
