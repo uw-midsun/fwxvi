82caa41cc69f99da2c80139f9ec94c8ea37a6c6b
vagrant <vagrant@midsunbox>
2025-07-19 19:57:01 +0000

    FLASHING WORKS

--- platform/hardware/templates/stm32_flash_procs.tcl
+++ platform/hardware/templates/stm32_flash_procs.tcl
@@ -1,22 +1,28 @@
-# This file can be used to automatically program the STM32L4 board's Flash memory from the command line
-# After programming, the board must be power-cycled (briefly remove the power) for the program to start running
-# This can be done by unpluggin the USB cable, or removing JP2
+# STM32L4 OpenOCD Flash Script
+# Flashes various firmware regions using OpenOCD + ST-Link
+# Automatically erases, writes, and verifies
+
+# Required global variables to define:
+# set ::FLASH_START
+# set ::FLASH_END
+# set ::BOOTSTRAP_SIZE
+# set ::BOOTLOADER_SIZE
+# set ::APP_ACTIVE_SIZE
+# set ::APP_STAGING_SIZE
 
 init
 
 proc stm_flash_legacy {IMGFILE} {
-	reset halt
-	sleep 100
-	wait_halt 2
+    reset halt
+    sleep 100
+    wait_halt 2
 
-    set FLASH_START $::FLASH_START
+    puts "Flashing app (legacy): Start address is $::FLASH_START"
 
-	flash write_image erase $IMGFILE FLASH_START
-	sleep 100
+    flash write_image erase $IMGFILE $::FLASH_START
+    verify_image $IMGFILE $::FLASH_START
 
-	verify_image $IMGFILE FLASH_START
-	sleep 100
-	reset run
+    reset run
 }
 
 proc stm_flash_bootstrap {IMGFILE} {
@@ -24,18 +30,15 @@ proc stm_flash_bootstrap {IMGFILE} {
     sleep 100
     wait_halt 2
 
-    set BOOTSTRAP_START $::FLASH_START
-    set BOOTSTRAP_END [expr {$BOOTSTRAP_START + $::BOOTSTRAP_SIZE}]
-    set END_SECTOR [expr {($::BOOTSTRAP_SIZE / 1024) - 1}]
+    set SECTOR_SIZE 2048
+    set START_ADDR $::FLASH_START
+    set END_ADDR   [expr {$START_ADDR + $::BOOTSTRAP_SIZE}]
+    set START_SECTOR 0
+    set END_SECTOR [expr {($END_ADDR - $START_ADDR) / $SECTOR_SIZE - 1}]
 
-    flash erase_sector 0 0 $END_SECTOR
-    sleep 100
-
-    flash write_image $IMGFILE $BOOTSTRAP_START
-    sleep 100
-
-    verify_image $IMGFILE $BOOTSTRAP_START
-    sleep 100
+    flash erase_sector 0 $START_SECTOR $END_SECTOR
+    flash write_image $IMGFILE $START_ADDR
+    verify_image $IMGFILE $START_ADDR
     reset run
 }
 
@@ -44,18 +47,16 @@ proc stm_flash_bootloader {IMGFILE} {
     sleep 100
     wait_halt 2
 
-    set BOOTLOADER_START [expr {$::FLASH_START + $::BOOTSTRAP_SIZE}]
-    set START_SECTOR [expr {$::BOOTSTRAP_SIZE / 1024}]
-    set END_SECTOR [expr {($::BOOTSTRAP_SIZE + $::BOOTLOADER_SIZE)/1024 - 1}]
+    set SECTOR_SIZE 2048
+
+    set START_ADDR [expr {$::FLASH_START + $::BOOTSTRAP_SIZE}]
+    set END_ADDR   [expr {$START_ADDR + $::BOOTLOADER_SIZE}]
+    set START_SECTOR [expr {($START_ADDR - $::FLASH_START) / $SECTOR_SIZE}]
+    set END_SECTOR   [expr {($END_ADDR   - $::FLASH_START) / $SECTOR_SIZE - 1}]
 
     flash erase_sector 0 $START_SECTOR $END_SECTOR
-    sleep 100
-
-    flash write_image $IMGFILE $BOOTLOADER_START
-    sleep 100
-
-    verify_image $IMGFILE $BOOTLOADER_START
-    sleep 100
+    flash write_image $IMGFILE $START_ADDR
+    verify_image $IMGFILE $START_ADDR
     reset run
 }
 
@@ -64,63 +65,23 @@ proc stm_flash_app_active {IMGFILE} {
     sleep 100
     wait_halt 2
 
-    set APP_ACTIVE_START [expr {
+    set SECTOR_SIZE 2048
+
+    set START_ADDR [expr {
         $::FLASH_START +
         $::BOOTSTRAP_SIZE +
-        $::BOOTLOADER_SIZE +
-        $::APP_ACTIVE_SIZE
+        $::BOOTLOADER_SIZE
     }]
 
-    set START_SECTOR [expr {
-        ($::BOOTSTRAP_SIZE + $::BOOTLOADER_SIZE + $::APP_ACTIVE_SIZE) / 1024
-    }]
+    set END_ADDR [expr {$START_ADDR + $::APP_ACTIVE_SIZE}]
+    set START_SECTOR [expr {($START_ADDR - $::FLASH_START) / $SECTOR_SIZE}]
+    set END_SECTOR   [expr {($END_ADDR - $::FLASH_START) / $SECTOR_SIZE - 1}]
 
-    set END_SECTOR [expr {
-        ($::FLASH_END - $APP_ACTIVE_START) / 1024 +
-        $START_SECTOR - 1
-    }]
+    puts "Flashing App Active: sectors $START_SECTOR to $END_SECTOR"
 
     flash erase_sector 0 $START_SECTOR $END_SECTOR
-    sleep 100
-
-    flash write_image $IMGFILE $APP_ACTIVE_START
-    sleep 100
-
-    verify_image $IMGFILE $APP_ACTIVE_START
-    sleep 100
-    reset run
-}
-
-proc stm_flash_app_staging {IMGFILE} {
-    reset halt
-    sleep 100
-    wait_halt 2
-
-    set APP_STAGING_START [expr {
-        $::FLASH_START +
-        $::BOOTSTRAP_SIZE +
-        $::BOOTLOADER_SIZE +
-        $::APP_ACTIVE_SIZE +
-        $::APP_STAGING_SIZE
-    }]
-
-    set START_SECTOR [expr {
-        ($::BOOTSTRAP_SIZE + $::BOOTLOADER_SIZE + $::APP_ACTIVE_SIZE + $::APP_STAGING_SIZE) / 1024
-    }]
-
-    set END_SECTOR [expr {
-        ($::FLASH_END - $APP_STAGING_START) / 1024 +
-        $START_SECTOR - 1
-    }]
-
-    flash erase_sector 0 $START_SECTOR $END_SECTOR
-    sleep 100
-
-    flash write_image $IMGFILE $APP_STAGING_START
-    sleep 100
-
-    verify_image $IMGFILE $APP_STAGING_START
-    sleep 100
+    flash write_image $IMGFILE $START_ADDR
+    verify_image $IMGFILE $START_ADDR
     reset run
 }
 
@@ -129,38 +90,31 @@ proc stm_flash_fs_storage {IMGFILE} {
     sleep 100
     wait_halt 2
 
-    set FS_STORAGE_START [expr {
+    set SECTOR_SIZE 2048
+
+    set START_ADDR [expr {
         $::FLASH_START +
         $::BOOTSTRAP_SIZE +
         $::BOOTLOADER_SIZE +
         $::APP_ACTIVE_SIZE +
-        $::APP_STAGING_SIZE +
-        $::FS_STORAGE_SIZE
+        $::APP_STAGING_SIZE
     }]
 
-    set START_SECTOR [expr {
-        ($::BOOTSTRAP_SIZE + $::BOOTLOADER_SIZE + $::APP_ACTIVE_SIZE + $::APP_STAGING_SIZE + $::FS_STORAGE_SIZE) / 1024
-    }]
+    set END_ADDR $::FLASH_END
+    set START_SECTOR [expr {($START_ADDR - $::FLASH_START) / $SECTOR_SIZE}]
+    set END_SECTOR   [expr {($END_ADDR   - $::FLASH_START) / $SECTOR_SIZE - 1}]
 
-    set END_SECTOR [expr {
-        ($::FLASH_END - $FS_STORAGE_START) / 1024 +
-        $START_SECTOR - 1
-    }]
+    puts "Flashing FS Storage: sectors $START_SECTOR to $END_SECTOR"
 
     flash erase_sector 0 $START_SECTOR $END_SECTOR
-    sleep 100
-
-    flash write_image $IMGFILE $FS_STORAGE_START
-    sleep 100
-
-    verify_image $IMGFILE $FS_STORAGE_START
-    sleep 100
+    flash write_image $IMGFILE $START_ADDR
+    verify_image $IMGFILE $START_ADDR
     reset run
 }
 
 proc stm_erase {} {
-	reset halt
-	sleep 100
-	stm32l4x mass_erase 0
-	sleep 100
+    reset halt
+    sleep 100
+    stm32l4x mass_erase 0
+    sleep 100
 }
