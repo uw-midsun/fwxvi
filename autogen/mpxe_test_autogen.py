25b3d229609600404aceed144ddac30bce39ea1d
Daiyan-Zubaier <daiyan102345@gmail.com>
2025-07-27 15:27:17 -0400

    Init: Defined file structure

---
+++ autogen/mpxe_test_autogen.py
@@ -0,0 +1,11 @@
+## @file    mpxe_test_autogen.py
+#  @date    2025-07-27
+#  @author  Midnight Sun Team #24 - MSXVI
+#  @brief   Writing soon # TODO
+#
+#  @details Writing soon # TODO
+#
+#  @ingroup autogen
+
+def get_data(args):
+    
