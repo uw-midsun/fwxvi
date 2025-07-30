f9a5bcac859669be0092ea81034cc30d76226b2b
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-07-30 20:11:48 +0000

    Set up x86 ltdc impl

--- libraries/gui/src/x86/ltdc.c
+++ libraries/gui/src/x86/ltdc.c
@@ -13,3 +13,21 @@
 #include "ltdc.h"
 
 /* Intra-component Headers */
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH    480
+#define SCREEN_HEIGHT   272
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+static uint8_t *framebuffer = NULL;
+static uint32_t *clut = NULL;
+
+void ltdc_init_ltdc(void) {
+    return 0;
+}
+ 
+void ltdc_set_framebuffer(uint8_t *buffer);
+void ltdc_load_clut(void);
+void ltdc_draw(void);
