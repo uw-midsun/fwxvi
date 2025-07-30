afbcbb976410dca64c01decfe7c9fbb65989340b
Aditya Sen <a5sen@uwaterloo.ca>
2025-07-30 04:21:36 -0400

    Add header files for the current sensor

---
+++ libraries/ms-drivers/inc/current_acs37800_defs.h
@@ -0,0 +1,31 @@
+#pragma once
+
+/************************************************************************************************
+ * @file    current_acs37800_defs.h
+ *
+ * @brief   Current Sensor ACS37800 Defs
+ *
+ * @date    2025-07-30
+ * @author  Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup current_acs37800_defs
+ * @brief    current_acs37800_defs Firmware
+ * @{
+ */
+
+
+// reg map is on pg.38
+// https://www.allegromicro.com/-/media/files/datasheets/acs37800-datasheet.ashx
+typedef enum {
+ 
+} ACS37800_Registers;
+
+/** @} */
