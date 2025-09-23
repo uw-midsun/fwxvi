68093c4d45f597c6742d9dcb39e2698dd763da1e
henry <laogui309@gmail.com>
2025-09-23 17:17:16 +0000

    Skeletons

--- projects/rear_controller/test/test_rear_controller.c
+++ projects/rear_controller/test/test_rear_controller.c
@@ -14,13 +14,50 @@
 /* Inter-component Headers */
 #include "test_helpers.h"
 #include "unity.h"
+#include "rear_controller.h"
+#include "status.h"
+#include "gpio.h"
 
 /* Intra-component Headers */
-
 void setup_test(void) {}
 
 void teardown_test(void) {}
 
 void test_example(void) {
   TEST_ASSERT_TRUE(true);
+}
+
+//Mock storage for relay tests
+static RearControllerStorage test_rear_controller_storage;
+
+TEST_IN_TASK
+void test_relay_init(void)(
+  TEST_ASSERT_TRUE(test_rear_controller!=nullptr);
+  StatusCode status=relay_init(test_rear_controller_storage);
+  TEST_ASSET_EQUAL(STATUS_CODE_OK, status);
+
+  //Check relays are all closed
+  TEST_ASSERT_TRUE(test_rear_contoller.pos_relay_closed);
+  TEST_ASSERT_TRUE(test_rear_contoller.neg_relay_closed);
+  TEST_ASSERT_TRUE(test_rear_contoller.solar_relay_closed);
+
+)
+
+TEST_IN_TASK
+void test_relay_fault(void){
+  StatusCode status=relay_fault();
+  TEST_ASSET_EQUAL(STATUS_CODE_OK, status);
+
+  //Check relays are all open
+  TEST_ASSERT_FALSE(test_rear_contoller.pos_relay_closed);
+  TEST_ASSERT_FALSE(test_rear_contoller.neg_relay_closed);
+  TEST_ASSERT_FALSE(test_rear_contoller.solar_relay_closed);
+  TEST_ASSERT_FALSE(test_rear_contoller.motor_relay_closed);
+}
+
+
+void test_relay_solar_close(void){
+  StatusCode status=relay_solar_close();
+  TEST_ASSET_EQUAL(STATUS_CODE_OK, status);
+  TEST_ASSERT_TRUE(test_rear_contoller.solar_relay_closed);
 }
