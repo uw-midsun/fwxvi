c6e4cc831898a6400e0527e92a3a9499b728dd95
juliejyyang <juliejyyang@gmail.com>
2025-09-29 21:01:09 +0000

    Thermistor fixes

--- projects/rear_controller/inc/thermistor.h
+++ projects/rear_controller/inc/thermistor.h
@@ -1,13 +1,35 @@
 #pragma once
-#include <stddef.h>
+
+/************************************************************************************************
+ * @file   thermistor.h
+ *
+ * @brief  Header file for thermistor module
+ *
+ * @date   2025-09-29
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdint.h>
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup Rear_Controller
+ * @brief    Rear Controller Board Firmware
+ * @{
+ */
 
 typedef struct {
-    float resistance; 
+  float resistance;
 } ThermistorDataPoint;
 
 extern const ThermistorDataPoint thermistor_table[];
-extern const uint16_t thermistor_table_size;
 
+#define THERMISTOR_TABLE_SIZE 102
 
+uint16_t calculate_board_thermistor_temperature(uint16_t thermistor_voltage_mv);
 
-
+/** @} */
