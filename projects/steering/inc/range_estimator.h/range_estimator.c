24acf105cbbd1fd128466bb5e8216737e360dd25
Chris Huynh <christghuynh@gmail.com>
2025-10-08 18:30:13 -0400

    Changed volts to millivolts

--- projects/steering/inc/range_estimator.h/range_estimator.c
+++ projects/steering/inc/range_estimator.h/range_estimator.c
@@ -14,15 +14,14 @@
 /* Intra-component Headers */
 #include "range_estimator.h"
 
-#define MIN_CELL_VOLTAGE 2.5f
-#define VOLTAGE_DROP_PER_LAP 0.0242f
-#define DISTANCE_PER_LAP_KM 5.07f //track is 3.15miles which is 5.07km
+#define MIN_CELL_VOLTAGE 2500.00
+#define VOLTAGE_DROP_PER_LAP 24.02
+#define DISTANCE_PER_LAP_KM 5.07 //track is 3.15miles which is 5.07km
 
 float estimate_remaining_range_km(float read_voltage) {
     if (read_voltage < MIN_CELL_VOLTAGE) {
-        return 0.0f;
+        return 0.0;
     }
-
     float voltage_remaining = read_voltage - MIN_CELL_VOLTAGE; //calculate remaining voltage
 
     float laps_remaining = voltage_remaining / VOLTAGE_DROP_PER_LAP; //calculate remaining laps
