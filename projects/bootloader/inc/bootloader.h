8ecdc4caf9bbe211ff5578673b7fefe61c771e49
Daiyan Zubaier <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-10-18 13:50:12 -0400

    CAN Bootloader Rework (#118)
    * One commit is all it takes 🎵 Possibilities
    
    * Fault implementation

--- projects/bootloader/inc/bootloader.h
+++ projects/bootloader/inc/bootloader.h
@@ -18,48 +18,13 @@
 #include "bootloader_can.h"
 #include "bootloader_error.h"
 #include "bootloader_flash.h"
+#include "bootloader_memory_map.h"
 
 /**
  * @defgroup bootloader
  * @brief    bootloader Firmware
  * @{
  */
-extern uint32_t _flash_start;
-extern uint32_t _flash_size;
-extern uint32_t _flash_page_size;
-extern uint32_t _application_start;
-extern uint32_t _application_size;
-extern uint32_t _bootloader_start;
-extern uint32_t _bootloader_size;
-extern uint32_t _sram_start;
-extern uint32_t _sram_size;
-
-/** @brief  Flash start address as defined in the linkerscripts */
-#define FLASH_START_ADDRESS_LINKERSCRIPT ((uint32_t)&_flash_start)
-
-/** @brief  Flash size as defined in the linkerscripts */
-#define FLASH_SIZE_LINKERSCRIPT ((size_t)&_flash_size)
-
-/** @brief  Flash page size as defined in the linkerscripts */
-#define FLASH_PAGE_SIZE_LINKERSCRIPT ((size_t)&_flash_page_size)
-
-/** @brief  Application start address as defined in the linkerscripts */
-#define APPLICATION_START_ADDRESS ((uint32_t) & _application_start)
-
-/** @brief  Application size as defined in the linkerscripts */
-#define APPLICATION_SIZE ((size_t) & _application_size)
-
-/** @brief  Bootloader start address as defined in the linkerscripts */
-#define BOOTLOADER_START_ADDRESS ((uint32_t) & _bootloader_start)
-
-/** @brief  Bootloader size as defined in the linkerscripts */
-#define BOOTLOADER_SIZE ((size_t) & _bootloader_size)
-
-/** @brief  SRAM start address as defined in the linkerscripts */
-#define SRAM_START_ADDRESS ((uint32_t) & _sram_start)
-
-/** @brief  SRAM size as defined in the linkerscripts */
-#define SRAM_SIZE ((size_t) & _sram_size)
 
 /**
  * @brief   Bootloader State Machine
@@ -70,7 +35,7 @@ typedef enum {
   BOOTLOADER_START,             /**< Bootloader is processing a start message */
   BOOTLOADER_WAIT_SEQUENCING,   /**< Bootloader is waiting for the data sequencing packet */
   BOOTLOADER_DATA_RECEIVE,      /**< Bootloader is receiving streamed data and flashing it in 2048 byte chunks */
-  BOOTLOADER_JUMP_APP,          /**< Bootloader is prompted to jump to application defined by APPLICATION_START_ADDRESS */
+  BOOTLOADER_JUMP_APP,          /**< Bootloader is prompted to jump to application defined by APP_ACTIVE_START_ADDRESS */
   BOOTLOADER_FAULT,             /**< Bootloader is in fault state */
   BOOTLOADER_PING               /**< Bootloader is ready to start receiving data */
 } BootloaderStates;
@@ -86,26 +51,6 @@ typedef enum {
   /// @brief Bootloader ping should do group stuff
   BOOTLOADER_PING_PROJECT,
 } BootloaderPingStates;
-
-/**
- * @brief   Private Bootloader State Storage
- */
-typedef struct {
-  uintptr_t application_start;       /**< Application start address */
-  uintptr_t current_write_address;   /**< Current write address */
-  uint32_t bytes_written;            /**< Number of bytes written to flash */
-  uint32_t data_size;                /**< Data size (ie: Binary application size) */
-  uint32_t packet_crc32;             /**< Packet CRC32 if available */
-  uint16_t expected_sequence_number; /**< Next expected sequence number for validation */
-  uint16_t buffer_index;             /**< Data buffer index for correct reading/writing */
-  BootloaderPingStates ping_type;    /**< Ping state of bootloader */
-  uint8_t ping_data_len;             /**< Length of ping data */
-  BootloaderStates state;   /**< Internal state tracker */
-  BootloaderError error;    /**< Bootloader error tracker */
-  uint16_t target_nodes;    /**< Target MCU Ids */
-  bool first_byte_received; /**< Boolean flag to track if the first byte was received */
-  bool first_ping_received; /**< Boolean flag to track if the first ping was received */
-} BootloaderStateData;
 
 /**
  * @brief   Initialize the bootloader
