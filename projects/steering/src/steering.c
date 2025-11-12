e5b30614a1866a0b5b26f8df8ad8933df4ccdf39
Aryan Kashem <130705280+Akashem06@users.noreply.github.com>
2025-11-12 15:52:56 -0500

    Steering validation (#135)
    * Steering validation
    
    * Working steering
    
    * Formatting
    
    * Test code
    
    * SD card driver changes
    
    * Build fix
    
    * Format

--- projects/steering/src/steering.c
+++ projects/steering/src/steering.c
@@ -19,8 +19,11 @@
 /* Intra-component Headers */
 #include "button_led_manager.h"
 #include "button_manager.h"
+#include "buzzer.h"
+#include "display.h"
 #include "drive_state_manager.h"
 #include "light_signal_manager.h"
+#include "party_mode.h"
 #include "steering.h"
 #include "steering_hw_defs.h"
 
@@ -55,12 +58,16 @@ StatusCode steering_init(SteeringStorage *storage, SteeringConfig *config) {
   steering_storage = storage;
   steering_storage->config = config;
 
-  log_init();
   can_init(&s_can_storage, &s_can_settings);
   drive_state_manager_init();
   lights_signal_manager_init();
-  button_manager_init(steering_storage);
   button_led_manager_init(steering_storage);
+  button_manager_init(steering_storage);
+  buzzer_init();
+  party_mode_init(steering_storage);
+  display_init(steering_storage);
+
+  buzzer_play_startup();
 
   return STATUS_CODE_OK;
 }
