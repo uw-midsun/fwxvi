d8214d49e50df415089912bdd7ee4f3b402cd014
Aryan Kashem <130705280+Akashem06@users.noreply.github.com>
2025-11-12 15:52:56 -0500

    Steering validation (#135)
    * Steering validation
    
    * Working steering
    
    * Formatting
    
    * Test code
    
    * SD card driver changes
    
    * Build fix
    
    * Format

---
+++ projects/steering/inc/display.h
@@ -0,0 +1,35 @@
+#pragma once
+
+/************************************************************************************************
+ * @file    display.h
+ *
+ * @brief   Header file for display control
+ *
+ * @date    2025-07-28
+ * @author  Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+#include <stdbool.h>
+
+/* Inter-component Headers */
+#include "status.h"
+
+/* Intra-component Headers */
+#include "steering.h"
+
+/**
+ * @defgroup steering
+ * @brief    steering Firmware
+ * @{
+ */
+
+/**
+ * @brief   Initialize the display
+ * @param   storage Pointer to the SteeringStorage instance
+ * @return  STATUS_CODE_OK if initialized successfully
+ *          STATUS_CODE_INVALID_ARGS if an invalid parameter is passed in
+ */
+StatusCode display_init(SteeringStorage *storage);
+
+/** @} */
