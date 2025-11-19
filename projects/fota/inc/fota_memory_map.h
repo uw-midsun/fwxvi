dcd735bc3c0a26f0cfc64e7d60dbb9bc524fa6dc
Deacon Sham <deaconsham@icloud.com>
2025-11-19 06:27:56 +0000

    Add light signalling to front controller and steering.

--- projects/fota/inc/fota_memory_map.h
+++ projects/fota/inc/fota_memory_map.h
@@ -96,49 +96,49 @@ extern uint32_t _sram_start;
 extern uint32_t _sram_size;
 
 /** @brief  Flash start address as defined in the linkerscripts */
-#define FLASH_START_ADDRESS_LINKERSCRIPT ((uint32_t) & _flash_start)
+#define FLASH_START_ADDRESS_LINKERSCRIPT ((uint32_t)&_flash_start)
 
 /** @brief  Flash size as defined in the linkerscripts */
-#define FLASH_SIZE_LINKERSCRIPT ((size_t) & _flash_size)
+#define FLASH_SIZE_LINKERSCRIPT ((size_t)&_flash_size)
 
 /** @brief  Flash page size as defined in the linkerscripts */
-#define FLASH_PAGE_SIZE_LINKERSCRIPT ((size_t) & _flash_page_size)
+#define FLASH_PAGE_SIZE_LINKERSCRIPT ((size_t)&_flash_page_size)
 
 /** @brief  Application Active start address as defined in the linkerscripts */
-#define APP_ACTIVE_START_ADDRESS ((uint32_t) & _app_active_start)
+#define APP_ACTIVE_START_ADDRESS ((uint32_t)&_app_active_start)
 
 /** @brief  Application Active size as defined in the linkerscripts */
-#define APP_ACTIVE_SIZE ((size_t) & _app_active_size)
+#define APP_ACTIVE_SIZE ((size_t)&_app_active_size)
 
 /** @brief  Application Staging start address as defined in the linkerscripts */
-#define APP_STAGING_START_ADDRESS ((uint32_t) & _app_staging_start)
+#define APP_STAGING_START_ADDRESS ((uint32_t)&_app_staging_start)
 
 /** @brief  Application Staging size as defined in the linkerscripts */
-#define APP_STAGING_SIZE ((size_t) & _app_staging_size)
+#define APP_STAGING_SIZE ((size_t)&_app_staging_size)
 
 /** @brief  Bootloader start address as defined in the linkerscripts */
-#define BOOTLOADER_START_ADDRESS ((uint32_t) & _bootloader_start)
+#define BOOTLOADER_START_ADDRESS ((uint32_t)&_bootloader_start)
 
 /** @brief  Bootloader size as defined in the linkerscripts */
-#define BOOTLOADER_SIZE ((size_t) & _bootloader_size)
+#define BOOTLOADER_SIZE ((size_t)&_bootloader_size)
 
 /** @brief  Bootstrap start address as defined in the linkerscripts */
-#define BOOTSTRAP_START_ADDRESS ((uint32_t) & _bootstrap_start)
+#define BOOTSTRAP_START_ADDRESS ((uint32_t)&_bootstrap_start)
 
 /** @brief  Bootstrap size as defined in the linkerscripts */
-#define BOOTSTRAP_SIZE ((size_t) & _bootstrap_size)
+#define BOOTSTRAP_SIZE ((size_t)&_bootstrap_size)
 
 /** @brief  File System Storage start address as defined in the linkerscripts */
-#define FS_STORAGE_START_ADDRESS ((uint32_t) & _fs_storage_start)
+#define FS_STORAGE_START_ADDRESS ((uint32_t)&_fs_storage_start)
 
 /** @brief  File System Storage size as defined in the linkerscripts */
-#define FS_STORAGE_SIZE ((size_t) & _fs_storage_size)
+#define FS_STORAGE_SIZE ((size_t)&_fs_storage_size)
 
 /** @brief  SRAM start address as defined in the linkerscripts */
-#define SRAM_START_ADDRESS ((uint32_t) & _sram_start)
+#define SRAM_START_ADDRESS ((uint32_t)&_sram_start)
 
 /** @brief  SRAM size as defined in the linkerscripts */
-#define SRAM_SIZE ((size_t) & _sram_size)
+#define SRAM_SIZE ((size_t)&_sram_size)
 
 #endif
 
