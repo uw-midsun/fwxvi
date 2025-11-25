b8e4021b619cedc8430d85f70afee697574dec3c
Darren Liu <darrrenliu@gmail.com>
2025-11-24 22:13:00 -0500

    added precharge check to power_state.c

--- projects/front_controller/src/power_state.c
+++ projects/front_controller/src/power_state.c
@@ -9,13 +9,18 @@ static PowerState previous_power_state = POWER_STATE_INVALID;
 
 static void set_load_switches(){
     uint8_t bps_fault_from_rear = get_rear_controller_status_bps_fault();
-    uint8_t drive_state_from_rear = get_steering_buttons_drive_state();
-    
+    uint8_t drive_state_from_steering = get_steering_buttons_drive_state();
+    uint8_t is_precharge_complete_from_rear = get_battery_stats_B_motor_precharge_complete();
+
     if (bps_fault_from_rear == 1){
         frontControllerStorage->current_power_state = POWER_STATE_FAULT;
-    }else if (drive_state_from_rear == DRIVE_STATE_DRIVE || drive_state_from_rear == DRIVE_STATE_CRUISE || drive_state_from_rear == DRIVE_STATE_REVERSE){
-        frontControllerStorage->current_power_state = POWER_STATE_ENGAGED;
-    }else if(drive_state_from_rear == DRIVE_STATE_NEUTRAL){
+    }else if (drive_state_from_steering == DRIVE_STATE_DRIVE || drive_state_from_steering == DRIVE_STATE_CRUISE || drive_state_from_steering == DRIVE_STATE_REVERSE){
+        if (is_precharge_complete_from_rear == 1){
+            frontControllerStorage->current_power_state = POWER_STATE_ENGAGED;
+        }else{
+            frontControllerStorage->current_power_state = previous_power_state;
+        }
+    }else if(drive_state_from_steering == DRIVE_STATE_NEUTRAL){
         frontControllerStorage->current_power_state = POWER_STATE_IDLE;
     }
 
