10f7ad2e2cdc05e10b99ce04d38c353477b54727
foehammer127 <foehammer@disroot.org>
2025-11-25 23:36:12 -0500

    Fix formatter script syntax error, and reformat.

--- projects/front_controller/inc/front_lights_signal.h
+++ projects/front_controller/inc/front_lights_signal.h
@@ -9,13 +9,23 @@
  * @author  Midnight Sun Team #24 - MSXVI
  ************************************************************************************************/
 
+/* Standard library Headers */
 #include <stdbool.h>
 
+/* Inter-component Headers */
 #include "front_controller_hw_defs.h"
 #include "global_enums.h"
 #include "gpio.h"
+
+/* Intra-component Headers */
 #include "software_timer.h"
 #include "status.h"
+
+/**
+ * @defgroup front_lights_signal
+ * @brief    front_lights_signal Firmware
+ * @{
+ */
 
 #define FRONT_LIGHTs_BLINK_PERIOD_MS 600
 #define SYSTEM_CAN_MESSAGE_STEERING_BUTTONS 6
@@ -25,3 +35,4 @@
  * @param state which is the state recieved by the CAN message
  */
 void front_lights_signal_process_event(SteeringLightState new_state);
+/** @} */
