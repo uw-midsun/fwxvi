551dea4ba3ff4be03fb9c4bd2e36214b86d01258
k4vijay <k4vijay@uwaterloo.ca>
2026-01-04 03:10:19 -0500

    Copy over BMI323 driver, run RTOS task and transmit IMU readings over UART

---
+++ projects/telemetry/src/imu.c
@@ -0,0 +1,54 @@
+#include "bmi323.h"
+#include "imu.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "log.h" //For LOG_ERROR
+#include "datagram.h"
+#include <string.h> //For memcpy
+#include "uart.h"
+
+void imu_task (void *pvParameters)
+{
+    Bmi323Storage *storage = (Bmi323Storage*)pvParameters;
+
+    TickType_t xLastWakeTime;
+    const TickType_t xPeriod = pdMS_TO_TICKS(100);
+    xLastWakeTime = xTaskGetTickCount();
+    while (1)
+    {
+        vTaskDelayUntil(&xLastWakeTime, xPeriod);
+
+        StatusCode status = bmi323_update(storage);
+        if (status != STATUS_CODE_OK)
+        {
+            LOG_ERROR("status error");
+            continue;
+        }
+
+        //create buffer & sending over UART
+        uint8_t buffer[26];
+        buffer[0] = DATAGRAM_START_FRAME;
+        memcpy(&buffer[1], &storage->gyro.x, sizeof(float));
+        memcpy(&buffer[5], &storage->gyro.y, sizeof(float));
+        memcpy(&buffer[9], &storage->gyro.z, sizeof(float));
+        memcpy(&buffer[13], &storage->accel.x, sizeof(float));
+        memcpy(&buffer[17], &storage->accel.y, sizeof(float));
+        memcpy(&buffer[21], &storage->accel.z, sizeof(float));
+        buffer[25] = DATAGRAM_END_FRAME;
+
+        uart_tx(UART_PORT_1, buffer, sizeof(buffer));
+    }
+}
+
+StatusCode imu_init(Bmi323Storage *storage, Bmi323Settings *settings)
+{
+    storage->settings = settings;
+    StatusCode status = bmi323_init(storage);
+    if (status != STATUS_CODE_OK)
+    {
+        return status;
+    }
+    xTaskCreate(imu_task, "imu", 1024, storage, 1, NULL);
+    
+    return STATUS_CODE_OK;
+}
