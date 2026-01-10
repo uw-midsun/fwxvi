27219249a09d618c5f9e796c9a99e35d2bf2e5b7
Julie Yang <94724821+juliejyyang@users.noreply.github.com>
2026-01-10 15:31:51 -0500

    Front Controller CAN - add power manager telemetry (#151)
    * add power manager telemetry to front controller can
    
    * reformatted output groups, added include to power_manager.c
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Darren Liu <darrrenliu@gmail.com>

--- projects/front_controller/src/power_manager.c
+++ projects/front_controller/src/power_manager.c
@@ -17,6 +17,7 @@
 
 /* Intra-component Headers */
 #include "front_controller_hw_defs.h"
+#include "front_controller_setters.h"
 #include "power_manager.h"
 #include "power_outputs.h"
 
@@ -117,6 +118,21 @@ static uint16_t power_sense_hi_current_calc(uint16_t sampled_voltage) {
   return result;
 }
 
+static void power_manager_set_telemetry() {
+  set_fc_power_group_A_rev_cam_current(s_power_manager_storage.current_readings[REV_CAM]);
+  set_fc_power_group_A_telem_current(s_power_manager_storage.current_readings[TELEM]);
+  set_fc_power_group_A_steering_current(s_power_manager_storage.current_readings[STEERING]);
+  set_fc_power_group_A_driver_fan_current(s_power_manager_storage.current_readings[DRIVER_FAN]);
+
+  set_fc_power_group_B_horn_current(s_power_manager_storage.current_readings[HORN]);
+  set_fc_power_group_B_spare_current(s_power_manager_storage.current_readings[SPARE_1]);
+
+  set_fc_power_lights_group_brake_light_sig_current(s_power_manager_storage.current_readings[BRAKE_LIGHT]);
+  set_fc_power_lights_group_bps_light_sig_current(s_power_manager_storage.current_readings[BPS_LIGHT]);
+  set_fc_power_lights_group_right_sig_current(s_power_manager_storage.current_readings[RIGHT_SIG]);
+  set_fc_power_lights_group_left_sig_current(s_power_manager_storage.current_readings[LEFT_SIG]);
+}
+
 /************************************************************************************************
  * Public functions
  ************************************************************************************************/
@@ -179,6 +195,8 @@ StatusCode power_manager_run_current_sense(OutputGroup group) {
 
     delay_ms(10);
   }
+
+  power_manager_set_telemetry();
 
   return STATUS_CODE_OK;
 }
