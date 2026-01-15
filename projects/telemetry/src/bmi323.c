e4e04f23a77cfee3c36415fe849e39182b3ec94a
Darren Liu <darrrenliu@gmail.com>
2026-01-15 01:16:01 -0500

    deleted commented code

--- projects/telemetry/src/bmi323.c
+++ projects/telemetry/src/bmi323.c
@@ -52,9 +52,7 @@ static StatusCode s_set_multi_register(Bmi323Registers reg, uint16_t *data, uint
 static StatusCode get_gyroscope_data(Axes *gyro);
 static StatusCode get_accel_data(Axes *accel);
 
-// static StatusCode get_gyro_offset_gain(GyroGainOffsetValues *gyro_go_values);
 static StatusCode set_gyro_offset_gain(GyroGainOffsetValues *gyro_go_values);
-// static StatusCode get_accel_offset_gain(AccelGainOffsetValues *accel_go_values);
 static StatusCode set_accel_offset_gain(AccelGainOffsetValues *accel_go_values);
 
 static void s_calculate_accel_offset();
@@ -142,38 +140,7 @@ static StatusCode get_accel_data(Axes *accel) {
 
   return status;
 }
-/*
-static StatusCode get_gyro_offset_gain(GyroGainOffsetValues *gyro_go_values) {
-        uint16_t data[6U] = { 0U };
 
-        uint16_t gyro_off_x, gyro_off_y, gyro_off_z;
-        uint8_t gyro_gain_x, gyro_gain_y, gyro_gain_z;
-
-        if (gyro_go_values == NULL) return BMI3_E_NULL_PTR;
-
-        StatusCode result = s_get_multi_register(GYR_DP_OFF_X, data, 6);
-
-        if (result != STATUS_CODE_OK) return result;
-
-        gyro_off_x = data[0];
-        gyro_off_y = data[2];
-        gyro_off_z = data[4];
-
-        gyro_gain_x = (uint8_t) data[1];
-        gyro_gain_y = (uint8_t) data[3];
-        gyro_gain_z = (uint8_t) data[5];
-
-        gyro_go_values->gyro_offset_x = (uint16_t) (gyro_off_x & BMI3_GYR_DP_OFF_X_MASK);
-        gyro_go_values->gyro_offset_y = (uint16_t) (gyro_off_y & BMI3_GYR_DP_OFF_Y_MASK);
-        gyro_go_values->gyro_offset_z = (uint16_t) (gyro_off_z & BMI3_GYR_DP_OFF_Z_MASK);
-
-        gyro_go_values->gyro_gain_x = gyro_gain_x;
-        gyro_go_values->gyro_gain_y = gyro_gain_y;
-        gyro_go_values->gyro_gain_z = gyro_gain_z;
-
-        return result;
-}
-*/
 static StatusCode set_gyro_offset_gain(GyroGainOffsetValues *gyro_go_values) {
   uint16_t data[6U] = { 0U };
 
@@ -203,37 +170,6 @@ static StatusCode set_gyro_offset_gain(GyroGainOffsetValues *gyro_go_values) {
 
   return result;
 }
-
-// static StatusCode get_accel_offset_gain(AccelGainOffsetValues *accel_go_values) {
-//     uint16_t data[6U] = { 0U };
-//
-//     uint16_t accel_off_x, accel_off_y, accel_off_z;
-//     uint8_t accel_gain_x, accel_gain_y, accel_gain_z;
-//
-//     if (accel_go_values == NULL) return BMI3_E_NULL_PTR;
-//
-//     StatusCode result = s_get_multi_register(ACC_DP_OFF_X, data, 6);
-//
-//     if (result != STATUS_CODE_OK) return result;
-//
-//     accel_off_x = data[0];
-//     accel_off_y = data[2];
-//     accel_off_z = data[4];
-//
-//     accel_gain_x = (uint8_t) data[1];
-//     accel_gain_y = (uint8_t) data[3];
-//     accel_gain_z = (uint8_t) data[5];
-//
-//     accel_go_values->accel_offset_x = (uint16_t) (accel_off_x & BMI3_ACC_DP_DOFFSET_X_MASK);
-//     accel_go_values->accel_offset_y = (uint16_t) (accel_off_y & BMI3_ACC_DP_DOFFSET_Y_MASK);
-//     accel_go_values->accel_offset_z = (uint16_t) (accel_off_z & BMI3_ACC_DP_DOFFSET_Y_MASK);
-//
-//     accel_go_values->accel_gain_x = accel_gain_x;
-//     accel_go_values->accel_gain_y = accel_gain_y;
-//     accel_go_values->accel_gain_z = accel_gain_z;
-//
-//     return result;
-// }
 
 static StatusCode set_accel_offset_gain(AccelGainOffsetValues *accel_go_values) {
   uint16_t data[6U] = { 0U };
@@ -265,196 +201,6 @@ static StatusCode set_accel_offset_gain(AccelGainOffsetValues *accel_go_values) 
   return result;
 }
 
-/*
-
-EXT.GYR_SC_SELECT.sens_en
-EXT.GYR_SC_SELECT.offs_en
-if both are being calibrated, sens is first
-
-write 0b0 to change default configuration
-
-by default, results of the self-calibration are written to data path registers:
-    GYR_DP_DGAIN_X
-    GYR_DP_DGAIN_Y
-    GYR_DP_DGAIN_Z
-    GYR_DP_OFF_X
-    GYR_DP_OFF_Y
-    GYR_DP_OFF_Z
-
-    by setting EXT.GYR_SC_SELECT.apply_corr to 0b0, the update of the data path registers can be suppressed
-    sensitivity of motion detection can be configured through EXT.GYR_MOT_DET.slope
-
-    prerequisites for self calibration:
-        if FEATURE_IO1.state is 0b00, self calibration can be initiated
-        accelerometer must be in high performace mode
-        sample rate of accelerometer is preffered to be in the range of 25Hz to 200Hz
-        alternative sensor configurations must be disabled by:
-            ALT_ACC_CONF.alt_acc_mode
-            ALT_GYR_CONF.alt_gyr_mode
-        to 0b0
-
-    to execute self calibration:
-        write 0x0101 to the register CMD
-        state of self calibration can be determined by checking
-            FEATURE_IO1.sc_st_complete
-                0b0 -> ongoing
-                0b1 -> completed
-                    or
-            FEATURE_IO1.state
-                0b01 -> ongoing
-            FEATURE_IO1.error_status
-                0x5 -> completed
-        Success of the sef calibration
-            FEATURE_IO1.gyro_sc_result
-                0b1 -> success
-                0b0 -> failure
-*/
-
-// static StatusCode enable_feature_engine() {
-//  s_set_register(FEATURE_IO2, 0x012C);
-//  s_set_register(FEATURE_IO_STATUS, 0x0001);
-
-//  uint16_t feature_ctrl = 0b1;
-
-//  s_set_register(FEATURE_CTRL, feature_ctrl);
-
-//  uint16_t feat_state = 0b000;
-
-//  for (uint32_t i = 0U; i < 5000U; i++) {
-//     if ((feat_state & (0b1111)) == 0b001) {
-//       break;
-//     }
-//     s_get_register(FEATURE_IO1, &feat_state);
-//     s_get_register(FEATURE_IO1, &feat_state);
-//     delay_ms(10U);
-//   }
-
-//  uint8_t state = feat_state & (0b1111);
-
-//  if (state != 0b001) return STATUS_CODE_TIMEOUT;
-
-//  return STATUS_CODE_OK;  // should only return if we didnt timeout
-// }
-
-/*
-static StatusCode gyro_crt_calibration() {
-  StatusCode enable_status = enable_feature_engine();
-
-  if (enable_status == STATUS_CODE_OK) {
-    // poll int status register value
-    uint16_t int_status_int1;
-    uint16_t int_status_int2;
-
-    uint8_t data1[2] = { 0 };
-    uint8_t data2[2] = { 0 };
-
-    // s_get_multi_register(INT_STATUS_INT1, data1, 2);
-    // s_get_multi_register(INT_STATUS_INT2, data2, 2);
-
-    int_status_int1 = data1[1] << 8 | data1[0];
-    int_status_int2 = data2[1] << 8 | data2[0];
-
-    // s_get_register(INT_STATUS_INT1, &int_status_int1);
-    // s_get_register(INT_STATUS_INT2, &int_status_int2);
-
-    // could just get the second byte and shift it by 2
-    int_status_int1 &= (1 << 10);
-    int_status_int2 &= (1 << 10);
-    uint16_t timeout = 0;
-
-    while (int_status_int1 != 1 && int_status_int2 != 1) {
-      // s_get_multi_register(INT_STATUS_INT1, data1, 2);
-      // s_get_multi_register(INT_STATUS_INT2, data2, 2);
-
-      int_status_int1 = data1[1] << 8 | data1[0];
-      int_status_int2 = data2[1] << 8 | data2[0];
-      // s_get_register(INT_STATUS_INT1, &int_status_int1);
-      // s_get_register(INT_STATUS_INT2, &int_status_int2);
-
-      int_status_int1 &= (1 << 10);
-      int_status_int2 &= (1 << 10);
-      timeout++;
-      if (timeout > 500) {
-        return STATUS_CODE_TIMEOUT;
-      }
-    }
-
-    timeout = 0;
-
-    // read FEATURE_IO1 and check error status
-    uint16_t feat_state;
-    s_get_register(FEATURE_IO1, &feat_state);
-
-    // error status
-    feat_state &= 1111;
-
-    while (feat_state != 0x5) {
-      s_get_register(FEATURE_IO1, &feat_state);
-      feat_state &= 1111;
-    }
-
-    // run the calibration
-    s_set_register(BMI323_REG_CMD, 0x0101);
-
-    // wait until self calibration is complete
-    s_get_register(FEATURE_IO1, &feat_state);
-    feat_state &= (1 << 6);
-
-    while (feat_state != 0b1) {
-      s_get_register(FEATURE_IO1, &feat_state);
-      feat_state &= (1 << 6);
-    }
-
-    // poll int status register value
-    // s_get_multi_register(INT_STATUS_INT1, data1, 2);
-    // s_get_multi_register(INT_STATUS_INT2, data2, 2);
-
-    int_status_int1 = data1[1] << 8 | data1[0];
-    int_status_int2 = data2[1] << 8 | data2[0];
-    // s_get_register(INT_STATUS_INT1, &int_status_int1);
-    // s_get_register(INT_STATUS_INT2, &int_status_int2);
-    int_status_int1 &= (1 << 10);
-    int_status_int2 &= (1 << 10);
-
-    while (int_status_int1 != 1 && int_status_int2 != 1) {
-      // s_get_multi_register(INT_STATUS_INT1, data1, 2);
-      // s_get_multi_register(INT_STATUS_INT2, data2, 2);
-
-      int_status_int1 = data1[1] << 8 | data1[0];
-      int_status_int2 = data2[1] << 8 | data2[0];
-      // s_get_register(INT_STATUS_INT1, &int_status_int1);
-      // s_get_register(INT_STATUS_INT2, &int_status_int2);
-      int_status_int1 &= (1 << 10);
-      int_status_int2 &= (1 << 10);
-    }
-
-    // read FEATURE_IO1 and check sc_st_complete and gyro_sc_result
-    s_get_register(FEATURE_IO1, &feat_state);
-
-    feat_state &= (1 << 4);
-
-    while (feat_state != 1) {
-      s_get_register(FEATURE_IO1, &feat_state);
-      feat_state &= (1 << 4);
-    }
-
-    s_get_register(FEATURE_IO1, &feat_state);
-    feat_state &= (1 << 5);
-
-    while (feat_state != 1) {
-      s_get_register(FEATURE_IO1, &feat_state);
-      feat_state &= (1 << 5);
-    }
-
-  } else {
-    return STATUS_CODE_INCOMPLETE;
-  }
-
-  get_accel_offset_gain(&imu_storage->accel_go_values);
-
-  return STATUS_CODE_OK;
-}
-*/
 static void s_calculate_accel_offset() {
   int16_t accel_x_off = 0, accel_y_off = 0, accel_z_off = 0;
 
