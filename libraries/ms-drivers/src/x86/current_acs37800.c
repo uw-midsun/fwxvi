01333562b0066d6c09aeb36e6f5a74ea21c047d1
Eric <70604868+ericoletsgo@users.noreply.github.com>
2026-02-04 18:13:48 -0500

    Fw 479 current sense x86 driver (#173)
    * Add x86 driver and unit tests for ACS37800 current sensor
    
    - Add x86 driver with register sim
    - Add test setters for injecting values (current, voltage, power, flags)
    - Add unit tests for all driver functions
    - Moved ARM driver from src/ to src/arm/ to match build system convention
    - Fix bit masks: overcurrent 0x02, overvoltage 0x08, undervoltage 0x10
    - Note: Fix flag checks (== 1 -> != 0)
    
    * if only there was smth like a pre commit hook to remind me to scons format
    
    * Added macros for status register masks and updated ARM driver
    
    - ACS37800_MASK_FAULTOUT, ACS37800_MASK_OVERVOLTAGE, ACS37800_MASK_UNDERVOLTAGE added to defs
    - Fix bit masks in ARM driver (0x03/0x04/0x05 -> 0x02/0x08/0x10)
    - Fix flag checks from == 1 to if(converted_data)
    - Fix I2C address validation (> 127)
    
    * nitpick - commenting

--- libraries/ms-drivers/src/x86/current_acs37800.c
+++ libraries/ms-drivers/src/x86/current_acs37800.c
@@ -1,49 +1,50 @@
 /************************************************************************************************
  * @file   current_acs37800.c
  *
- * @brief  Source file to implement the ACS37800 current sensing driver
+ * @brief  x86 mock driver for the ACS37800 current sensing driver
  *
- * @date   2025-07-30
+ * @date   2025-01-28
  * @author Midnight Sun Team #24 - MSXVI
  ************************************************************************************************/
 
 /* Standard library Headers */
+#include <stddef.h>
 
 /* Inter-component Headers */
+#include "log.h"
+#include "status.h"
 
 /* Intra-component Headers */
 #include "current_acs37800.h"
-
 #include "current_acs37800_defs.h"
+#define ACS37800_NUM_REGISTERS 0x30
 
-// initialize the storage object
+static ACS37800Storage *s_storage = NULL;
+static uint32_t s_registers[ACS37800_NUM_REGISTERS] = { 0 };
+
 StatusCode acs37800_init(ACS37800Storage *storage, I2CPort i2c_port, I2CAddress i2c_address) {
-  if (storage == NULL || i2c_address < 127) {
+  if (storage == NULL || i2c_address > 127) {
     return STATUS_CODE_INVALID_ARGS;
   }
-  // i2c peripherals
+
   storage->i2c_port = i2c_port;
   storage->i2c_address = i2c_address;
+  s_storage = storage;
+
+  // Clear all registers
+  for (int i = 0; i < ACS37800_NUM_REGISTERS; i++) {
+    s_registers[i] = 0;
+  }
 
   return STATUS_CODE_OK;
 }
 
 StatusCode acs37800_get_register(ACS37800Storage *storage, ACS37800_Registers reg, uint32_t *out_raw) {
-  if (storage == NULL) {
+  if (storage == NULL || out_raw == NULL) {
     return STATUS_CODE_INVALID_ARGS;
   }
 
-  uint8_t reg_addr = (uint8_t)reg;
-  uint8_t rx_buff[4];
-
-  StatusCode status = i2c_read_mem(storage->i2c_port, storage->i2c_address, reg_addr, rx_buff, sizeof(rx_buff));
-
-  if (status != STATUS_CODE_OK) {
-    return status;
-  }
-
-  *out_raw = ((uint32_t)rx_buff[0] << 24) | ((uint32_t)rx_buff[1] << 16) | ((uint32_t)rx_buff[2] << 8) | ((uint32_t)rx_buff[3]);
-
+  *out_raw = s_registers[reg];
   return STATUS_CODE_OK;
 }
 
@@ -59,9 +60,8 @@ StatusCode acs37800_get_current(ACS37800Storage *storage, float *out_current_amp
     return status;
   }
 
-  // the current value is signed (16 bits upper)
+  // 16 bits upper
   int16_t current_raw = (int16_t)((raw_data >> 16) & 0xFFFF);
-
   *out_current_amps = (float)(current_raw)*CURRENT_SCALE;
 
   return STATUS_CODE_OK;
@@ -81,7 +81,6 @@ StatusCode acs37800_get_voltage(ACS37800Storage *storage, float *out_voltage_mV)
 
   // the voltage value is signed (16 bits lower)
   int16_t voltage_raw = (int16_t)(raw_data & 0xFFFF);
-
   *out_voltage_mV = (float)(voltage_raw)*VOLTAGE_SCALE;
 
   return STATUS_CODE_OK;
@@ -99,9 +98,8 @@ StatusCode acs37800_get_active_power(ACS37800Storage *storage, float *out_power_
     return status;
   }
 
-  // the active power value is signed (16 bits lower)
+  // active power signed 16bit lower
   int16_t power_raw = (int16_t)(raw_data & 0xFFFF);
-
   *out_power_mW = (float)(power_raw)*POWER_SCALE;
 
   return STATUS_CODE_OK;
@@ -119,9 +117,9 @@ StatusCode acs37800_get_overcurrent_flag(ACS37800Storage *storage, bool *overcur
     return status;
   }
 
-  uint32_t converted_data = raw_data & 0x00000003;
+  uint32_t converted_data = raw_data & ACS37800_MASK_FAULTOUT;
 
-  if (converted_data == 1) {
+  if (converted_data) {
     *overcurrent_flag = true;
   } else {
     *overcurrent_flag = false;
@@ -135,23 +133,9 @@ StatusCode acs37800_reset_overcurrent_flag(ACS37800Storage *storage) {
     return STATUS_CODE_INVALID_ARGS;
   }
 
-  uint32_t raw_data;
-  StatusCode status = acs37800_get_register(storage, ACS37800_REG_STATUS, &raw_data);
-  if (status != STATUS_CODE_OK) {
-    return status;
-  }
-
-  // write all of it back, reset the FAULTLATCH address
-  uint8_t tx_buff[4] = {
-    (uint8_t)((raw_data >> 24) & 0xFF),
-    (uint8_t)((raw_data >> 16) & 0xFF),
-    (uint8_t)((raw_data >> 8) & 0xFF),
-    (uint8_t)(raw_data & 0xFF),
-  };
-
-  status = i2c_write_reg(storage->i2c_port, storage->i2c_address, ACS37800_REG_STATUS, tx_buff, sizeof(tx_buff));
-
-  return status;
+  // In ARM writes to I2C, instead just clear the flag bits
+  s_registers[ACS37800_REG_STATUS] &= ~ACS37800_MASK_FAULTOUT;
+  return STATUS_CODE_OK;
 }
 
 StatusCode acs37800_get_overvoltage_flag(ACS37800Storage *storage, bool *overvoltage_flag) {
@@ -166,9 +150,9 @@ StatusCode acs37800_get_overvoltage_flag(ACS37800Storage *storage, bool *overvol
     return status;
   }
 
-  uint32_t converted_data = raw_data & 0x00000004;
+  uint32_t converted_data = raw_data & ACS37800_MASK_OVERVOLTAGE;
 
-  if (converted_data == 1) {
+  if (converted_data) {
     *overvoltage_flag = true;
   } else {
     *overvoltage_flag = false;
@@ -189,13 +173,71 @@ StatusCode acs37800_get_undervoltage_flag(ACS37800Storage *storage, bool *underv
     return status;
   }
 
-  uint32_t converted_data = raw_data & 0x00000005;
+  uint32_t converted_data = raw_data & ACS37800_MASK_UNDERVOLTAGE;
 
-  if (converted_data == 1) {
+  if (converted_data) {
     *undervoltage_flag = true;
   } else {
     *undervoltage_flag = false;
   }
 
   return STATUS_CODE_OK;
+}
+
+ACS37800Storage *acs37800_get_storage(void) {
+  return s_storage;
+}
+
+/* Setters */
+
+void acs37800_set_register(ACS37800_Registers reg, uint32_t value) {
+  s_registers[reg] = value;
+}
+
+void acs37800_set_current(float current_amps) {
+  // Encode into upper 16 bits of VCODES_ICODES register
+  int16_t current_raw = (int16_t)(current_amps / CURRENT_SCALE);
+  // Clear upper 16 bits
+  s_registers[ACS37800_REG_VCODES_ICODES] &= 0x0000FFFF;
+  s_registers[ACS37800_REG_VCODES_ICODES] |= ((uint32_t)current_raw << 16);
+}
+
+void acs37800_set_voltage(float voltage_mV) {
+  // Encode into lower 16 bits of VCODES_ICODES register
+  int16_t voltage_raw = (int16_t)(voltage_mV / VOLTAGE_SCALE);
+  // Clear lower 16 bits
+  s_registers[ACS37800_REG_VCODES_ICODES] &= 0xFFFF0000;
+  s_registers[ACS37800_REG_VCODES_ICODES] |= ((uint32_t)voltage_raw & 0xFFFF);
+}
+
+void acs37800_set_power(float power_mW) {
+  // Encode into lower 16 bits of PACTIVE_PIMAGE register
+  int16_t power_raw = (int16_t)(power_mW / POWER_SCALE);
+  // Clear lower 16 bits
+  s_registers[ACS37800_REG_PACTIVE_PIMAGE] &= 0xFFFF0000;
+  s_registers[ACS37800_REG_PACTIVE_PIMAGE] |= ((uint32_t)power_raw & 0xFFFF);
+}
+
+void acs37800_set_overcurrent_flag(bool flag) {
+  if (flag) {
+    s_registers[ACS37800_REG_STATUS] |= ACS37800_MASK_FAULTOUT;
+  } else {
+    s_registers[ACS37800_REG_STATUS] &= ~ACS37800_MASK_FAULTOUT;
+  }
+}
+
+void acs37800_set_overvoltage_flag(bool flag) {
+  if (flag) {
+    s_registers[ACS37800_REG_STATUS] |= ACS37800_MASK_OVERVOLTAGE;
+  } else {
+    s_registers[ACS37800_REG_STATUS] &= ~ACS37800_MASK_OVERVOLTAGE;
+  }
+}
+
+void acs37800_set_undervoltage_flag(bool flag) {
+  if (flag) {
+    s_registers[ACS37800_REG_STATUS] |= ACS37800_MASK_UNDERVOLTAGE;
+  } else {
+    s_registers[ACS37800_REG_STATUS] &= ~ACS37800_MASK_UNDERVOLTAGE;
+  }
 }
