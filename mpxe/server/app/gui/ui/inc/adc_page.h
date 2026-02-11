7b3dd3f8b1d10b52cbcab7a5a15c7712a3732657
Bernice Lam <bernicelam912@gmail.com>
2026-02-11 19:07:06 +0000

    in progress

---
+++ mpxe/server/app/gui/ui/inc/adc_page.h
@@ -0,0 +1,85 @@
+// #pragma once
+
+// /************************************************************************************************
+//  * @file    adc_page.h
+//  *
+//  * @brief   Adc Page
+//  *
+//  * @date    2025-02-09
+//  * @author  Midnight Sun Team #24 - MSXVI
+//  ************************************************************************************************/
+
+// /* Standard library headers */
+// #include <map>
+
+// /* Qt library headers */
+// #include <QPointer>
+// #include <QSortFilterProxyModel>
+// #include <QString>
+// #include <QTabWidget>
+// #include <QVariant>
+// #include <QWidget>
+
+// /* Inter-component headers */
+
+// /* Intra-component headers */
+
+// /**
+//  * @defgroup MPXE_GUI
+//  * @brief    Gui model for MPXE
+//  * @{
+//  */
+
+// /**
+//  * @class   AdcPage
+//  * @brief   Page showing ADC channel key, gpio port, and reading
+//  * @details Builds two tabs (raw readings & converted readings) with a searchable table from the "adc" map in the payload:
+//  *          raw readings: {
+//  *            "channel 11": { "Gpio Port": "...", "Reading": "..." },
+//  *            ...
+//  *          }
+//  */
+
+// class AdcPage : public QWidget {
+//   Q_OBJECT
+
+//  public:
+//   /**
+//    * @brief   Construct a new AdcPage widget
+//    * @param   payload Initial payload containing "adc" map
+//    * @param   parent  Parent widget (can be nullptr)
+//    */
+//   explicit AdcPage(const std::map<QString, QVariant> &payload, QWidget *parent = nullptr);
+
+//  public slots:
+//   /**
+//    * @brief   Replace the current payload and rebuild the UI
+//    * @param   payload New payload map
+//    */
+//   void setPayload(const std::map<QString, QVariant> &payload);
+
+//  private:
+//   /**
+//    * @brief   Rebuilds the entire UI from m_payload
+//    * @details Clears all tabs, extracts the ADC map, and rebuilds the table.
+//    */
+//   void rebuild();
+
+//   /**
+//    * @brief   Extracts a nested QVariantMap as std::map<QString,QVariant>
+//    * @param   input_map   Root map
+//    * @param   key_wanted  Key whose nested QVariantMap should be converted
+//    * @return  std::map<QString, QVariant> Flattened version of the nested QVariantMap
+//    */
+//   std::map<QString, QVariant> extractMap(const std::map<QString, QVariant> &input_map, const QString &key_wanted) const;
+
+//  private:
+//   std::map<QString, QVariant> m_payload; /**< Current payload for this page */
+//   QPointer<QTabWidget> m_tabs;           /**< Tab widget container for tables */
+
+//   QSortFilterProxyModel *m_raw_proxy; /**< Proxy for raw readings table (can be used for filtering) */
+//   QSortFilterProxyModel *m_conv_proxy;      /**< Proxy model for the converted readings table */
+  
+// };
+
+// /** @} */
---
+++ mpxe/server/app/gui/ui/src/adc_page.cc
@@ -0,0 +1,119 @@
+// /************************************************************************************************
+//  * @file    adc_page.cc
+//  *
+//  * @brief   Adc Page
+//  *
+//  * @date    2025-08-30
+//  * @author  Midnight Sun
+//  ************************************************************************************************/
+
+// /* Standard library headers */
+
+// /* Qt library headers */
+// #include <QFormLayout>
+// #include <QHeaderView>
+// #include <QTableView>
+// #include <QVBoxLayout>
+
+// /* Inter-component headers */
+// #include "dict_table_model.h"
+// #include "utils.h"
+// #include "voltage_table_model.h"
+
+// /* Intra-component headers */
+// #include "adc_page.h"
+
+// /**
+//  * @brief Extract maps from afe{...} ex: main_pack, thermistor_temperature, and so on
+//  */
+// inline std::map<QString, QVariant> extractMapInline(const std::map<QString, QVariant> &input_map, const QString &key_wanted) {
+//   std::map<QString, QVariant> out;
+//   std::map<QString, QVariant>::const_iterator it = input_map.find(key_wanted);
+
+//   if (it != input_map.end() && it->second.canConvert<QVariantMap>()) {
+//     const QVariantMap v_map = it->second.toMap();
+//     QVariantMap::const_iterator v_it = v_map.constBegin();
+
+//     for (; v_it != v_map.constEnd(); ++v_it) {
+//       out[v_it.key()] = v_it.value();
+//     }
+//   }
+//   return out;
+// }
+
+// AfePage::AfePage(const std::map<QString, QVariant> &payload, QWidget *parent) :
+//     QWidget{ parent }, m_payload{ payload }, m_tabs{ new QTabWidget(this) }, m_discharge_proxy{ nullptr }, m_pack_proxy{ nullptr }, m_therm_proxy{ nullptr } {
+//   QVBoxLayout *layout = new QVBoxLayout(this);
+//   layout->setContentsMargins(0, 0, 0, 0);
+//   layout->addWidget(m_tabs);
+//   rebuild();
+// }
+
+// void AfePage::setPayload(const std::map<QString, QVariant> &payload) {
+//   m_payload = payload;
+//   rebuild();
+// }
+
+// std::map<QString, QVariant> AfePage::extractMap(const std::map<QString, QVariant> &input_map, const QString &key_wanted) const {
+//   return extractMapInline(input_map, key_wanted);
+// }
+
+// void AfePage::rebuild() {
+//   m_tabs->clear();
+
+//   const std::map<QString, QVariant> discharge_map = extractMap(m_payload, QStringLiteral("cell_discharge"));
+//   const std::map<QString, QVariant> main_pack_map = extractMap(m_payload, QStringLiteral("main_pack"));
+//   const std::map<QString, QVariant> therm_map = extractMap(m_payload, QStringLiteral("thermistor_temperature"));
+//   const std::map<QString, QVariant> board_map = extractMap(m_payload, QStringLiteral("board_thermistors"));
+
+//   /* Discharge */
+//   {
+//     DictTableModel *model = new DictTableModel(discharge_map, false, m_tabs);
+//     TableWithSearch tws = makeSearchableTable(model, m_tabs);
+//     m_discharge_proxy = tws.proxy;
+//     m_tabs->addTab(tws.widget, QStringLiteral("Cell Discharge"));
+//   }
+
+//   /* Main Pack */
+//   {
+//     VoltageTableModel *model = new VoltageTableModel(main_pack_map, MIN_VOLTAGE, MAX_VOLTAGE, m_tabs);
+//     TableWithSearch tws = makeSearchableTable(model, m_tabs);
+//     m_pack_proxy = tws.proxy;
+
+//     if (tws.table) {
+//       tws.table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
+//       tws.table->horizontalHeader()->setSectionResizeMode(1, QHeaderView::ResizeToContents);
+//       tws.table->horizontalHeader()->setSectionResizeMode(2, QHeaderView::ResizeToContents);
+//       tws.table->horizontalHeader()->setSectionResizeMode(3, QHeaderView::Fixed);
+//       tws.table->setColumnWidth(3, 80);
+//     }
+//     m_tabs->addTab(tws.widget, QStringLiteral("Main Pack"));
+//   }
+
+//   /* Thermistors */
+//   {
+//     DictTableModel *model = new DictTableModel(therm_map, false, m_tabs);
+//     TableWithSearch tws = makeSearchableTable(model, m_tabs);
+//     m_therm_proxy = tws.proxy;
+
+//     if (tws.table) {
+//       tws.table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
+//       tws.table->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+//       tws.table->verticalHeader()->setVisible(false);
+//     }
+//     m_tabs->addTab(tws.widget, QStringLiteral("Thermistors"));
+//   }
+
+//   /* Board Thermistors as separate tab */
+//   {
+//     DictTableModel *model = new DictTableModel(board_map, false, m_tabs);
+//     TableWithSearch tws = makeSearchableTable(model, m_tabs);
+
+//     if (tws.table) {
+//       tws.table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
+//       tws.table->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+//       tws.table->verticalHeader()->setVisible(false);
+//     }
+//     m_tabs->addTab(tws.widget, QStringLiteral("Board Thermistors"));
+//   }
+// }
