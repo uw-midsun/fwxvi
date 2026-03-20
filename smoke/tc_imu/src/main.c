3a4c150c5c8a2ddea913dce94843bd61ba95c10f
jun li <junchenli423@gmail.com>
2026-03-19 20:07:03 -0400

    Resolved IMU PR comments

--- smoke/tc_imu/src/main.c
+++ smoke/tc_imu/src/main.c
@@ -29,16 +29,6 @@ TelemetryStorage telemetry_storage;
 
 TelemetryConfig telemetry_config = {
   .message_transmit_frequency_hz = 1000U,
-  .uart_port = TELEMETRY_XBEE_UART_PORT,
-  .uart_settings = { .tx = GPIO_TELEMETRY_UART_TX, .rx = GPIO_TELEMETRY_UART_RX, .baudrate = TELEMETRY_XBEE_UART_BAUDRATE, .flow_control = TELEMETRY_XBEE_UART_FLOW_CONTROL },
-  .sd_spi_port = SPI_PORT_2,
-  .sd_spi_settings = { .baudrate = SD_SPI_BAUDRATE_2_5MHZ,
-                       .mode = SD_SPI_MODE_1,
-                       .mosi = GPIO_TELEMETRY_SPI_MOSI,
-                       .miso = GPIO_TELEMETRY_SPI_MISO,
-                       .sclk = GPIO_TELEMETRY_SPI_SCK,
-                       .cs = GPIO_TELEMETRY_SPI_NSS,
-                      },
 };
 
 Bmi323Settings bmi323_settings = {
@@ -62,15 +52,18 @@ TASK(IMU_RUN, 1024) {
   imu_init(&bmi323_storage, &bmi323_settings);
 
   telemetry_storage.bmi323_storage = &bmi323_storage;
+  delay_ms(1000U);
+  StatusCode status;
+
   while (true) {
     LOG_DEBUG("Starting IMU run\n");
-    StatusCode status = imu_run();
+    status = imu_run();
 
     if (status != STATUS_CODE_OK) {
       LOG_DEBUG("Error occurred");
       delay_ms(1000U);
     } else if (status == STATUS_CODE_OK) {
-      LOG_DEBUG("IMU gx=%.3f gy=%.3f gz=%.3f ax=%.3f ay=%.3f az=%.3f\n", (double)bmi323_storage.gyro.x, (double)bmi323_storage.gyro.y, (double)bmi323_storage.gyro.z, (double)bmi323_storage.accel.x,
+      LOG_DEBUG("IMU gx=%.3f gy=%.3f gz=%.3f ax=%.3f ay=%.3f az=%.3f\r\n", (double)bmi323_storage.gyro.x, (double)bmi323_storage.gyro.y, (double)bmi323_storage.gyro.z, (double)bmi323_storage.accel.x,
                 (double)bmi323_storage.accel.y, (double)bmi323_storage.accel.z);
       delay_ms(1000U);
     }
