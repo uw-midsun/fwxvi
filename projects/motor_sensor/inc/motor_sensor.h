f28cdeaed2582667f5050ed06b8cdb3b349f4714
Darren Liu <darrrenliu@gmail.com>
2026-03-22 20:39:12 -0400

    start of motor sensor

---
+++ projects/motor_sensor/inc/motor_sensor.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/************************************************************************************************
+ * @file   motor_sensor.h
+ *
+ * @brief  Header file for motor_sensor
+ *
+ * @date   2026-03-22
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+
+/* Intra-component Headers */
+
+/**
+ * @defgroup motor_sensor
+ * @brief    motor_sensor Firmware
+ * @{
+ */
+
+/** @} */
