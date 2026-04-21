af21d9c00787e9a4a4420b670708793cefd07789
Lorenzo Good <44120040+lorenzogood@users.noreply.github.com>
2026-04-20 22:30:02 -0700

    Properly toggle debug logging using scons defines. (#206)
    * Disable uart logging by default due to related slowness.
    
    * Fix little skill issue
    
    * [auto-format] SCons Format and Lint via Github Actions
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- libraries/ms-common/inc/log.h
+++ libraries/ms-common/inc/log.h
@@ -93,7 +93,7 @@ extern UartSettings log_uart_settings;
 
 #ifdef MS_PLATFORM_X86
 #define LOG(level, fmt, ...) printf("[%u] %s:%u: " fmt, (level), __FILE__, __LINE__, ##__VA_ARGS__)
-#elif MS_LOG == 1
+#elif MS_DEBUG_LOG
 #define LOG(level, fmt, ...)                                                                                                            \
   do {                                                                                                                                  \
     if (xTaskGetSchedulerState() == taskSCHEDULER_RUNNING) {                                                                            \
