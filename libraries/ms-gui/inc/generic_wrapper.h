e0cf900879ae8deb35ba7dd66a848afa6f2adde6
vagrant <vagrant@midsunbox>
2026-05-20 05:27:09 +0000

    scons lint

--- libraries/ms-gui/inc/generic_wrapper.h
+++ libraries/ms-gui/inc/generic_wrapper.h
@@ -8,8 +8,7 @@
  * @date   2026-05-12
  * @author Midnight Sun Team #24 - MSXVI
  ********
-*/
-
+ */
 
 #include "lvgl.h"
 
@@ -17,29 +16,26 @@
  * @brief Configuration struct for a generic LVGL widget.
  * All settings for the widget are defined here.
  */
-typedef struct{
-    int32_t x;
-    int32_t y;
-    int32_t width;
-    int32_t height;
-    const lv_image_dsc_t *img_src;
-    lv_obj_t *parent;
-    lv_color_t color;
-}widget_config_t;
+typedef struct {
+  int32_t x;
+  int32_t y;
+  int32_t width;
+  int32_t height;
+  const lv_image_dsc_t *img_src;
+  lv_obj_t *parent;
+  lv_color_t color;
+} widget_config_t;
 
 /**
  * @brief Represents a generic widget instance.
  */
-typedef struct{
-    lv_obj_t *obj;
-    widget_config_t* config;
-}widget_t;
+typedef struct {
+  lv_obj_t *obj;
+  widget_config_t *config;
+} widget_t;
 
-widget_t create_widget(widget_config_t* config);
+widget_t create_widget(widget_config_t *config);
 
-void set_image(const lv_image_dsc_t* img, widget_t *widget);
+void set_image(const lv_image_dsc_t *img, widget_t *widget);
 
 void set_color(const lv_color_t color, widget_t *widget);
-
-
-
