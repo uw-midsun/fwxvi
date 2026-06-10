f41e94895ea51802828f3bf6b70abc62079cc5e4
Sophie <sophiesee20@gmail.com>
2026-06-10 18:56:06 -0400

    June 10

--- libraries/ms-drivers/inc/current_ads122c14irter.h
+++ libraries/ms-drivers/inc/current_ads122c14irter.h
@@ -19,7 +19,6 @@
 #include "status.h"
 
 /* Intra-component Headers */
-#include "current_acs37800_defs.h"
 
 /**
  * @defgroup ADS122C14ITER Current Sensing
@@ -27,24 +26,29 @@
  * @{
  */
 
-
  // this is a 24 bit guy
 
 //# define I2CAddress ADC_ADDRESS = 0x40; /* A0 and A1 are both wired to GND*/
 
+#define ADS122_NUM_REGS = 16U
+
+#define _PACKED __attribute__((packed))   
+
+#define REG_MAP_CRC_VALUES = 0
 
 typedef struct {
   I2CPort i2c_port;
   I2CAddress i2c_address;
+  uint8_t register_map[ADS122_NUM_REGS];
 } ADS122Storage;
 
-typedef enum : uint8_t{
+typedef enum : uint8_t {
   ADS122_REG_DEVICE_ID = 0b00000000,
   ADS122_REG_REVISION_ID = 0b00000001,
   ADS122_REG_STATUS_MSB = 0b00000010,
   ADS122_REG_STATUS_LSB = 0b00000011,
   ADS122_REG_CONVERSION_CTRL = 0b00000100,
-  ADS122_REG_DEVICE_CFG = 0b00000101,
+  ADS122_REG_DEVICE_CFG = 0b00000101, /* Start init configs here*/
   ADS122_REG_DATA_RATE_CFG = 0b00000110,
   ADS122_REG_MUX_CFG = 0b00000111,
   ADS122_REG_GAIN_CFG = 0b00001000,
@@ -57,8 +61,6 @@ typedef enum : uint8_t{
   ADS122_REG_REG_MAP_CRC = 0b00001111,
 } ADS122C14ITER_Register;
 
-#define ADS122_NUM_REGS = 16U;
-
 typedef enum : uint8_t{
   ADS122_WRITE_COMMAND = 0b10000000,
   ADS122_READ_COMMAND = 0b01000000,
@@ -68,20 +70,250 @@ typedef enum : uint8_t{
 _Static_assert(sizeof(ADS122C14ITER_Register) == 1U );
 _Static_assert(sizeof(ADS122C14ITER_Command) == 1U );
 
+// Do I need to include any of the earlier ones?
+
+/*ADS122_REG_STATUS_MSB*/
+#define ADS122_RESET_BITOFFSET 7
+#define ADS122_RESET_MASK (1 << 7)
+
+#define ADS122_AVDD_UV_BITOFFSET 6
+#define ADS122_AVDD_UV_MASK (1 << 6)
+
+#define ADS122_REF_UV_BITOFFSET 5
+#define ADS122_REF_UV_MASK (1 << 5)
+
+#define ADS122_REG_MAP_CRC_FAULT_BITOFFSET 3
+#define ADS122_REG_MAP_CRC_FAULT_MASK (1 << 3)
+
+#define ADS122_MEM_FAULT_BITOFFSET 2
+#define ADS122_REG_MAP_CRC_FAULT_MASK (1 << 2)
+
+#define ADS122_REG_WRITE_FAULT_BITOFFSET 1
+#define ADS122_REG_MAP_CRC_FAULT_MASK (1 << 1)
+
+#define ADS122_DRDY_BITOFFSET 0
+#define ADS122_REG_MAP_CRC_FAULT_MASK (1 << 0)
+
+/* ADS122_REG_STATUS_LSB*/
+#define ADS122_CONV_COUNT_BITOFFSET 4 //should this be 4 or 7
+#define ADS122_GPIO3_DAT_IN_MASK (1 << 7) |  (1 << 6) | (1 << 5) | (1 << 4)
+
+#define ADS122_GPIO3_DAT_IN 3
+#define ADS122_GPIO3_DAT_IN_MASK (1 << 3)
+
+#define ADS122_GPIO2_DAT_IN 2
+#define ADS122_GPIO2_DAT_IN_MASK (1 << 2)
+
+#define ADS122_GPIO1_DAT_IN_BITOFFSET 1
+#define ADS122_GPIO1_DAT_IN_MASK (1 << 1)
+
+#define ADS122_GPIO0_DAT_IN_BITOFFSET 0
+#define ADS122_GPIO0_DAT_IN_MASK (1 << 0)
+
+/*ADS122_REG_CONVERSION_CTRL*/
+#define ADS122_RESET_BITOFFSET 2
+#define ADS122_RESET_MASK (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2)
+
+#define ADS122_START_BITOFFSET 1
+#define ADS122_START_MASK (1 << 1)
+
+#define ADS122_STOP_BITOFFSET 0
+#define ADS122_STOP_MASK (1 << 0)
+
+/*ADS122_REG_DEVICE_CFG*/
+//which speed??
+#define ADS122_REG_DEVICE_CFG_DEFAULT ((uint8_t) 0b00000100)
+
+#define ADS122_PWDN_BITOFFSET 7
+#define ADS122_PWDN_MASK (1 << 7)
+
+#define ADS122_STBY_MODE_BITOFFSET 6
+#define ADS122_STBY_MODE_MASK (1 << 6)
+
+#define ADS122_BOCS_BITOFFSET 4
+#define ADS122_BOCS_MASK (1 << 5) | (1 << 4)
+
+#define ADS122_CLK_SEL_BITOFFSET 3
+#define ADS122_CLK_SEL_MASK (1 << 3)
+
+#define ADS122_CONV_MODE_BITOFFSET 2
+#define ADS122_CONV_MODE_MASK (1 << 2)
+
+#define ADS122_SPEED_MODE_BITOFFSET 0
+#define ADS122_SPEED_MODE_MASK (1 << 1) | (1 << 0)
+
+/* ADS122_REG_DATA_RATE_CFG*/
+#define ADS122_REG_DATA_RATE_CFG_DEFAULT ((uint8_t) 0x00)
+
+#define ADS122_DELAY_BITOFFSET 4
+#define ADS122_DELAY_MASK (1 << 7) | (1 << 6) || (1 << 5) || (1 << 4)
+
+#define ADS122_GC_EN_BITOFFSET 3
+#define ADS122_GC_EN_MASK (1 << 3)
+
+#define ADS122_FLTR_OSR_BITOFFSET 0
+#define ADS122_FLTR_OSR_MASK (1 << 2) | (1 << 1) | (1 << 0) 
+
+/* ADS122_REG_MUX_CFG*/
+#define ADS122_REG_MUX_CFG_DEFAULT ((uint8_t) 0x01)
+
+#define ADS122_AINP_BITOFFSET 4
+#define ADS122_AINP_MASK (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4)
+
+#define ADS122_AINN_BITOFFSET 0
+#define ADS122_AINN_MASK (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0)
+
+
+/*ADS122_REG_GAIN_CFG*/
+#define ADS122_REG_GAIN_CFG_DEFAULT ((uint8_t) 0x01)
+
+#define ADS122_SYS_MON_BITOFFSET 4
+#define ADS122_SYS_MON_MASK (1 << 6) | (1 << 5) | (1 << 4)
+
+#define ADS122_GAIN_BITOFFSET 0
+#define ADS122_GAIN_MASK (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0)
+
+/*ADS122_REG_REFERENCE_CFG */
+#define ADS122_REG_REFERENCE_CFG_DEFAULT ((uint8_t) 0x00)
+
+#define ADS122_REF_UV_EN_BITOFFSET 7
+#define ADS122_REF_UV_EN_MASK (1 << 7)
+
+#define ADS122_REFP_BUF_EN_BITOFFSET 5
+#define ADS122_REFP_BUF_EN_MASK (1 << 5)
+
+#define ADS122_REFN_BUF_EN_BITOFFSET 4
+#define ADS122_REFN_BUF_EN_MASK (1 << 4)
+
+#define ADS122_REF_VAL_BITOFFSET 2
+#define ADS122_REF_VAL_MASK (1 << 2)
+
+#define ADS122_REF_SEL_BITOFFSET 0
+#define ADS122_REF_SEL_MASK (1 << 1) | (1 << 0)
+
+/*ADS122_REG_DIGITAL_CFG*/
+#define ADS122_REG_DIGITAL_CFG_DEFAULT ((uint8_t) 0x00)
+
+#define ADS122_REG_MAP_CRC_EN_BITOFFSET 6
+#define ADS122_REG_MAP_CRC_EN_MASK (1 << 6)
+
+#define ADS122_I2C_CRC_EN_BITOFFSET 5
+#define ADS122_I2C_CRC_EN_MASK (1 << 5)
+
+#define ADS122_STATUS_EN_BITOFFSET 4
+#define ADS122_STATUS_EN_MASK (1 << 4)
+
+#define ADS122_FAULT_PIN_BEHAVIOR_BITOFFSET 3
+#define ADS122_FAULT_PIN_BEHAVIOR_MASK (1 << 3)
+
+#define ADS122_CODING_BITOFFSET 1
+#define ADS122_CODING_MASK (1 << 1)
+
+/*ADS122_REG_GPIO_CFG*/
+#define ADS122_REG_GPIO_CFG_DEFAULT ((uint8_t) 0x00)
+
+#define ADS122_GPIO3_CFG_BITOFFSET 6
+#define ADS122_GPIO3_CFG_MASK  (1 << 7) |(1 << 6)
+
+#define ADS122_GPIO2_CFG_BITOFFSET 4
+#define ADS122_GPIO2_CFG_MASK (1 << 5) | (1 << 4)
+
+#define ADS122_GPIO1_CFG_BITOFFSET 2
+#define ADS122_GPIO1_CFGR_MASK (1 << 3) | (1 << 2)
+
+#define ADS122_GPIO0_CFG_BITOFFSET 0
+#define ADS122_GPIO0_CFG_MASK (1 << 1) | (1 << 0)
+
+/*ADS122_REG_GPIO_DATA_OUTPUT*/
+#define ADS122_REG_GPIO_DATA_OUTPUT_DEFAULT ((uint8_t) 0x00)
+
+#define ADS122_GPIO3_SRC_BITOFFSET 7
+#define ADS122_GPIO3_SRC_MASK (1 << 7)
+
+#define ADS122_GPIO2_SRC_BITOFFSET 6
+#define ADS122_GPIO2_SRC_MASK (1 << 6)
+
+#define ADS122_GPIO3_DAT_OUT_BITOFFSET 3
+#define ADS122_GPIO3_DAT_OUT_MASK (1 << 3)
+
+#define ADS122_GPIO2_DAT_OUT_BITOFFSET 2
+#define ADS122_GPIO2_DAT_OUT_MASK (1 << 2)
+
+#define ADS122_GPIO1_DAT_OUT_BITOFFSET 1
+#define ADS122_GPIO1_DAT_OUT_MASK (1 << 1)
+
+#define ADS122_GPIO0_DAT_OUT_BITOFFSET 0
+#define ADS122_GPIO0_DAT_OUT_MASK (1 << 0)
+
+/*ADS122_REG_IDAC_MAG_CFG*/
+#define ADS122_REG_IDAC_MAG_CFG_DEFAULT ((uint8_t) 0x00)
+
+#define ADS122_I2MAG_BITOFFSET 4
+#define ADS122_I2MAG_MASK (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4)
+
+#define ADS122_I1MAG_BITOFFSET 0
+#define ADS122_I1MAG_MASK (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0)
+
+//shift left by 4 for I2MUX
+typedef enum : uint8_t{
+  Disabled = 0,
+  Iuint_1x,
+  Iuint_10x,
+  Iuint_20x,
+  Iuint_30x,
+  Iuint_40x,
+  Iuint_50x,
+  Iuint_60x,
+  Iuint_70x,
+  Iuint_80x,
+  Iuint_90x,
+  Iuint_100x,
+  Iuint_100x,
+  Iuint_100x,
+  Iuint_100x,
+  Iuint_100x,
+} ADS122_IUINT;
+
+#define ADS122_IUNIT_Length 4
+
+/*ADS122_REG_IDAC_MUX_CFG*/
+#define ADS122_REG_IDAC_MUX_CFG_DEFAULT ((uint8_t) 0x10)
+//check the default IDAC2 output pin selection
+
+#define ADS122_IUNIT_BITOFFSET 7
+#define ADS122_IUNIT_MASK (1 << 7)
+
+#define ADS122_I2MUX_BITOFFSET 4
+#define ADS122_I2MUX_MASK (1 << 6) | (1 << 5) | (1 << 4)
+
+#define ADS122_I1MUX_BITOFFSET 0
+#define ADS122_I1MUX_MASK (1 << 2) | (1 << 1) | (1 << 0)
+
+//shift left by 3 for I2MUX
+typedef enum : uint8_t{
+  AIN0 = 0,
+  AIN1,
+  AIN2,
+  AIN3,
+  AIN4,
+  AIN5,
+  AIN6,
+  AIN7,
+} ADS122_AIN_pins;
+
+#define ADS122_Ain_pins_length 3
+
+/*ADS122_REG_REG_MAP_CRC*/
+#define ADS122_REG_REG_MAP_CRC_DEFAULT ((uint8_t) 0x00)
 
 
 
+// /* Registers*/
+// /**
+//  * Device ID
+//  * STATUS_MSB
+//  * Device config regs
+//  * CONVERSION_CTRL
+//  */
 
-
-
-
-
-/* Registers*/
-/**
- * Device ID
- * STATUS_MSB
- * Device config regs
- * CONVERSION_CTRL
- */
-
-/* What timing mode? <- timing requirements*/
+// /* What timing mode? <- timing requirements*/
--- libraries/ms-drivers/src/arm/current_ads122c14irter.c
+++ libraries/ms-drivers/src/arm/current_ads122c14irter.c
@@ -15,46 +15,60 @@
 /* Intra-component Headers */
 #include "current_ads122c14irter.h";
 
-static uint8_t ads122_reg_config[ADS122_NUM_REGS];
 
-StatusCode init_acs122(){
+//static uint8_t ads122_reg_config[ADS122_NUM_REGS];
 
+#define ADS122_READ_CONVERSION_LENGTH 2U;
+
+//static ADS122Storage storage;
+
+//let the user take the 
+
+StatusCode ads122_init(ADS122Storage * storage, I2CPort i2c_port_storage, I2CAddress i2c_address_storage, uint8_t ads122_reg_config[]){
+    storage->i2c_port = i2c_port_storage;
+    storage->i2c_address = i2c_address_storage;
+
+    /* Ensure the device is powered on and communicating*/
+    uint8_t device_id;
+    status_ok_or_return(ads122_read_register(&storage, device_id, ADS122_REG_DEVICE_ID));
+    if(device_id == 0){
+        return STATUS_CODE_UNREACHABLE;
+    }
+
+    /* Reset RESETn and AVDD_UVn fault flags*/
+    uint8_t reset_status_msb = 0xC0;
+    status_ok_or_return(ads122_write_register(&storage, reset_status_msb, ADS122_REG_STATUS_MSB));
+
+    /* Set init configs */
+
+    // Set default configs
+    for(int i = ADS122_REG_DEVICE_CFG; i < ADS122_NUM_REGS; i++){
+        status_ok_or_return(ads122_write_register(&storage, ads122_reg_config[i-ADS122_REG_DEVICE_CFG], i));
+    }
+
+    ads122_start_conversion(storage);
 
     return STATUS_CODE_OK;
-
 }
+
+void ads122_change_reg_config();
 
 /* Combines the command and the register*/
-static uint8_t acs122_create_command(ADS122C14ITER_Register reg, ADS122C14ITER_Command command){
+static uint8_t ads122_create_command(ADS122C14ITER_Register reg, ADS122C14ITER_Command command){
     return command || reg;
-}
-
-/* Combines the command and the register, adds the to a data output array <- Should we add the data the reg wants as well?*/
-static StatusCode acs122_create_command(ADS122C14ITER_Register reg, ADS122C14ITER_Command command, uint8_t *data[], uint16_t element){
-    data[element] = acs122_create_command( reg, command);
-    return STATUS_CODE_OK;
-}
-
-/* Combines the command and the register, adds to a specified spot in the data output (write specifically) then adds data to write*/
-static StatusCode acs122_create_write_command(ADS122C14ITER_Register reg, ADS122C14ITER_Command command, uint8_t *tx_data[], uint16_t element, uint8_t *data[]){
-    tx_data[element] = acs122_create_command( reg, command);
-    memcpy(tx_data + (element + 1), data, sizeof(data));
-    return STATUS_CODE_OK;
 }
 
 //how big should the read data be
 //read protocol: S-target-WRITE --ACK-- RREG --ACK-- Sr-Target-READ --ACK--DATA-- (-ACK- --CRC--) -ACK-STOP-
 /* Reads a specified register and gets the data */
-static StatusCode acs122_read_register(ADS122Storage * storage, uint8_t *data, ADS122C14ITER_Register reg){
-    if(&storage == NULL){
+static StatusCode ads122_read_register(ADS122Storage * storage, uint8_t *data, ADS122C14ITER_Register reg){
+    if(storage == NULL){
         return STATUS_CODE_INVALID_ARGS;
     } 
 
-   uint8_t reg_address = acs122_create_command(reg, ADS122_READ_COMMAND);
+   uint8_t reg_address = ads122_create_command(reg, ADS122_READ_COMMAND);
 
-    uint8_t rx_data;
-
-    StatusCode status_read = i2c_read_mem(storage->i2c_port, storage->i2c_address, reg_address, &rx_data, sizeof(rx_data));
+    StatusCode status_read = i2c_read_mem(storage->i2c_port, storage->i2c_address, reg_address, &data, 1U);
     if(status_read != STATUS_CODE_OK){
         return status_read;
     }
@@ -62,11 +76,15 @@ static StatusCode acs122_read_register(ADS122Storage * storage, uint8_t *data, A
     return STATUS_CODE_OK;
 }
 
-/* Writes to ONE register -> data needs to be in the right format already*/
-static StatusCode acs122_write_register(ADS122Storage * storage, uint8_t* data[]){
-    if(&storage == NULL){
+/* Writes to ONE register, will combine data and register*/
+
+//ADS122_REG_WRITE_FAULT <- use to check for faults
+static StatusCode ads122_write_register(ADS122Storage * storage, uint8_t *data, ADS122C14ITER_Register reg){
+    if(storage == NULL){
         return STATUS_CODE_INVALID_ARGS;
     }
+
+    uint8_t tx_data [] = {ads122_create_command(reg, ADS122_WRITE_COMMAND), data};
 
     StatusCode status = i2c_write(storage->i2c_port, storage->i2c_address, &data, 2U);
     if(status != STATUS_CODE_OK){
@@ -76,38 +94,70 @@ static StatusCode acs122_write_register(ADS122Storage * storage, uint8_t* data[]
     return STATUS_CODE_OK;
 }
 
-/* Writes to ONE register, will combine data and register*/
-static StatusCode acs122_write_register(ADS122Storage * storage, uint8_t *data, ADS122C14ITER_Register reg){
-    uint8_t tx_data [] = {acs122_create_command(reg, ADS122_WRITE_COMMAND), data};
-    
-    return acs122_write_register(&storage, &tx_data);
-}
-
 /* Writes to a series of register - 8 bits for command - 8 bits for data*/
-static StatusCode acs122_write_register(ADS122Storage * storage, uint8_t *data[], ADS122C14ITER_Register reg[], uint32_t data_length){
-    if(&storage == NULL){
+static StatusCode ads122_write_multiple_registers(ADS122Storage * storage, uint8_t *data[], ADS122C14ITER_Register reg[], uint32_t data_length, uint8_t *tx_data){
+    if(storage == NULL){
         return STATUS_CODE_INVALID_ARGS;
     } 
 
-    // TODO: is there a max length that can be sent (or should be sent)
-    data_length *= 2;
+    for (uint32_t i = 0; i < data_length; i++) {
+        tx_data[i * 2] = ads122_create_command(reg[i], ADS122_WRITE_COMMAND);
+        tx_data[i * 2 + 1] = data[i];
+    }
 
-    //size of doesn't work on pointers
-    uint8_t tx_data[sizeof(&data)];
+    return i2c_write(storage->i2c_port, storage->i2c_address, tx_data, data_length * 2U);
+}
 
-    StatusCode status = i2c_write(storage->i2c_port, storage->i2c_address, &data, 1U);
+static StatusCode ads122_read_conversion(ADS122Storage *storage, uint8_t *data[])
+{
+    if (storage == NULL || data == NULL) {
+        return STATUS_CODE_INVALID_ARGS;
+    }
+
+    uint8_t command = ADS122_READ_CONVERSION_COMMAND;
+    StatusCode status = i2c_read_mem(storage->i2c_port, storage->i2c_address, command,  data, ADS122_READ_CONVERSION_LENGTH);
     if(status != STATUS_CODE_OK){
         return status;
     }
+    return STATUS_CODE_OK;
+}
 
+static StatusCode ads122_start_conversion(ADS122Storage * storage){
+    uint8_t conversion_ctrl;
+    status_ok_or_return(ads122_read_register(storage, conversion_ctrl, ADS122_REG_CONVERSION_CTRL));
+    conversion_ctrl |= (1 << 1);
+    status_ok_or_return(ads122_write_register(storage, &conversion_ctrl, ADS122_REG_CONVERSION_CTRL));
+    return STATUS_CODE_OK;
+}
+
+static StatusCode ads122_stop_conversion(ADS122Storage * storage){
+    uint8_t conversion_ctrl;
+    status_ok_or_return(ads122_read_register(storage, conversion_ctrl, ADS122_REG_CONVERSION_CTRL));
+    conversion_ctrl |= (1 << 0);
+    status_ok_or_return(ads122_write_register(storage, &conversion_ctrl, ADS122_REG_CONVERSION_CTRL));
     return STATUS_CODE_OK;
 }
 
 
 
-// Writing can write to multiple registers by putting another WREG frame in
+/* things for init*/
+
+// Set default for the array
+
+
+// bool true/ false (on / off) ->
+// get the bit
+// get the register
+
 //what is the maximum data that can be passed in?? -> should be checked
 
+//need to configure GPIO, what conversion mode
+
+// single poll mode (not continuous)
+
+// how to make sure you're getting new stuff
+// - poll the DRDY bit -> 1b means the conversion data is new
+// do we want continuous data even if it is old???
 
 //write to regs
     //get data
@@ -116,7 +166,9 @@ static StatusCode acs122_write_register(ADS122Storage * storage, uint8_t *data[]
 
 // get data
 
-//the crc thing
+// no crc, no status -> bare bones
+//read is wrong, ne
+
 //is new data ready?
 
 
