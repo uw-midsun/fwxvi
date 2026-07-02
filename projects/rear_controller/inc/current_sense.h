792727bfb6d71a8227ab8d9ea0b2e4d3922fbe76
Sophie <sophiesee20@gmail.com>
2026-07-02 14:08:01 -0400

    updated conversion

--- projects/rear_controller/inc/current_sense.h
+++ projects/rear_controller/inc/current_sense.h
@@ -25,12 +25,7 @@
  * @{
  */
 
-#define IS_USING_CURRENT_SENSE_REV_3 1U
 
-#if (IS_USING_CURRENT_SENSE_REV_3 != 0U)
-
-
-#else
 /**
  * @brief   Initializes the current sense sub-system
  * @param   storage Pointer to the rear controller storage
@@ -46,5 +41,4 @@ StatusCode current_sense_init(RearControllerStorage *rear_controller_storage);
  */
 StatusCode current_sense_run();
 
-#endif
 /** @} */
