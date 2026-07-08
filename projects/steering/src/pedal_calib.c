fd1f26faea3152a4da769e6d2fb249e9b5a22fd4
Darren <darrrenliu@gmail.com>
2026-07-07 23:32:31 -0400

    scons format

--- projects/steering/src/pedal_calib.c
+++ projects/steering/src/pedal_calib.c
@@ -68,7 +68,7 @@ StatusCode steering_pedal_calib_init(SteeringStorage *storage) {
   if (storage == NULL) {
     return STATUS_CODE_INVALID_ARGS;
   }
-  
+
   set_pedal_calib_request_command(0);
 
   s_calib_state.active = false;
