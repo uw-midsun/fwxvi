bdff135f265d544bfc80d6c3863e94d3b21ba79e
Darren <darrrenliu@gmail.com>
2026-07-20 23:30:23 -0400

    comment out disable bps

--- projects/steering/src/steering.c
+++ projects/steering/src/steering.c
@@ -115,7 +115,7 @@ StatusCode steering_init(SteeringStorage *storage, SteeringConfig *config, Ws22M
   steering_pedal_calib_init(steering_storage);
 
   buzzer_play_startup();
-  steering_force_disable_bps();
+  // steering_force_disable_bps();
 
   // button_led_manager_clear_all();
   return STATUS_CODE_OK;
