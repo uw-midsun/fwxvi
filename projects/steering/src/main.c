06c22366d057b34a7afce0bede8772487b3bb753
Lorenzo Good <foehammer@disroot.org>
2026-07-29 10:52:14 -0400

    Mul 100

--- projects/steering/src/main.c
+++ projects/steering/src/main.c
@@ -63,7 +63,7 @@ void run_10hz_cycle() {
   drive_state_manager_update();
   lights_signal_manager_update();
   cruise_control_run_medium_cycle();
-  set_steering_cruise_control_target_velocity((uint32_t)(steering_storage.target_velocity));
+  set_steering_cruise_control_target_velocity((uint32_t)(steering_storage.target_velocity * 100.0f));
   run_can_tx_medium();
 }
 
